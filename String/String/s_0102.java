0bf3fc2f66a2dadc9bfc64400bb09ac04efe3dc0
gayoung choi <cy614@naver.com>
2022-03-31 16:47:50 +0900

    알고리즘

---
+++ String/String/s_0102.java
@@ -0,0 +1,12 @@
+package String;
+
+public class s_0102 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 1;
+		int b = 2;
+		System.out.println(a+b);
+	}
+
+}
