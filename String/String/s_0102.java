a9359da33af62363abacff9c557f7ad0b1a58f69
gayoung choi <cy614@naver.com>
2022-03-31 17:16:23 +0900

    대소문자 변환

--- String/String/s_0102.java
+++ String/String/s_0102.java
@@ -1,11 +1,20 @@
 package String;
 
-public class s_0102 {
+import java.util.Scanner;
 
+public class s_0102 {
+	public String solution(String str) {
+		String answer = "";
+		for(char x : str.toCharArray()) { //string을 문자열 배열로 변환
+			if(Character.isLowerCase(x)) answer+=Character.toUpperCase(x); //isLowerCase = character가 소문자일경우
+			else answer+=Character.toLowerCase(x); //소문자->대문자 대문자->소문자
+		}
+		return answer;
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int a = 1;
-		int b = 2;
-		System.out.println(a+b);
+		s_0102 T = new s_0102();
+		Scanner kb = new Scanner(System.in);
+		String str = kb.next();
+		System.out.println(T.solution(str));
 	}
 }
