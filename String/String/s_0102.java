1969df3fbd90d8c1b10a5cd8bb8c5281e92ec214
gayoung choi <cy614@naver.com>
2022-03-31 17:37:24 +0900

    대소문자 변환문제 정수형 풀이 추가

--- String/String/s_0102.java
+++ String/String/s_0102.java
@@ -6,8 +6,11 @@ public class s_0102 {
 	public String solution(String str) {
 		String answer = "";
 		for(char x : str.toCharArray()) { //string을 문자열 배열로 변환
-			if(Character.isLowerCase(x)) answer+=Character.toUpperCase(x); //isLowerCase = character가 소문자일경우
-			else answer+=Character.toLowerCase(x); //소문자->대문자 대문자->소문자
+			/* if(Character.isLowerCase(x)) answer+=Character.toUpperCase(x); //isLowerCase = character가 소문자일경우
+			else answer+=Character.toLowerCase(x); //소문자->대문자 대문자->소문자 */
+			//정수형으로 풀이 대문자는 65~90 소문자는 97~122 
+			if(x>= 97 && x<=122) answer+=(char)(x-32);
+			else answer+=(char)(x+32); //문자형으로 cast 필요 
 		}
 		return answer;
 	}
