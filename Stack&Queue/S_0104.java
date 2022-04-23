65f173373f97aa82929cfd5f078832e6135a6ce3
gayoung choi <cy614@naver.com>
2022-04-23 23:37:29 +0900

    후위식 연산(postfix)

---
+++ Stack&Queue/S_0104.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+//후위식 연산(postfix)
+public class S_0104 {
+	public int solution(String str) {
+		int answer = 0;
+		Stack<Integer> stack = new Stack<>();
+		for(char x : str.toCharArray()) {
+			if(Character.isDigit(x)) stack.push(x-48);
+			else {
+				int rt = stack.pop();
+				int lt = stack.pop();
+				if(x =='+') stack.push(lt+rt);
+				else if(x == '-') stack.push(lt-rt);
+				else if(x == '*') stack.push(lt*rt);
+				else if(x == '/') stack.push(lt/rt);
+			}
+		}
+		answer = stack.get(0);
+		return answer;
+	}
+	
+	public static void main(String[] args) {
+		S_0104 T = new S_0104();
+		Scanner kb = new Scanner(System.in);
+		String str = kb.next();
+		System.out.println(T.solution(str));
+		
+	}
+
+}
