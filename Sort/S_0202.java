0e0a658b994a9679cd41d09f5cbd76a7f770ec1b
gayoung choi <cy614@naver.com>
2022-04-27 11:51:19 +0900

    버블 정렬

---
+++ Sort/S_0202.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+//버블 정렬 
+public class S_0202 {
+	public int[] solution(int n, int[] arr) {
+		for(int i = 0; i<n-1; i++) {
+			for (int j = 0; j < n-i-1; j++) {
+				if(arr[j]>arr[j+1]) {
+					int tmp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = tmp;
+				}
+			}
+		}
+			return arr;
+		}
+	
+	public static void main(String[] args) {
+		S_0202 T = new S_0202();
+		Scanner kb = new Scanner(System.in);
+		int n = kb.nextInt();
+		int[] arr = new int[n];
+		for(int i = 0; i<n; i++) arr[i]= kb.nextInt();
+		for(int x : T.solution(n, arr)) System.out.print(x+" ");
+	}
+
+}
