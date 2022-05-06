2a1ddcc9586b5dd7e9065f6856932fdc35985994
gayoung choi <cy614@naver.com>
2022-05-06 22:14:52 +0900

    피보나치 재귀(Memorization)

--- DFS_BFS/theory/S_0104.java
+++ DFS_BFS/theory/S_0104.java
@@ -2,8 +2,9 @@ package theory;
 
 //피보나치 재귀(중요) 
 public class S_0104 {
-	static int[] fibo;
+	static int[] fibo; //배열
 	public int DFS(int n) {
+		if(fibo[n]>0) return fibo[n]; //배열의 값을 메모 
 		if(n==1) return fibo[n] = 1;
 		else if(n==2) return fibo[n] = 1;
 		else return fibo[n] = DFS(n-2)+DFS(n-1);
