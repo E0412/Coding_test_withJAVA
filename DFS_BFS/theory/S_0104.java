ef7a72d360b78420e22b1c16ff982f2f548403a6
gayoung choi <cy614@naver.com>
2022-05-06 22:09:38 +0900

    피보나치 재귀(배열로 개선)

--- DFS_BFS/theory/S_0104.java
+++ DFS_BFS/theory/S_0104.java
@@ -2,15 +2,18 @@ package theory;
 
 //피보나치 재귀(중요) 
 public class S_0104 {
+	static int[] fibo;
 	public int DFS(int n) {
 		if(n==1) return 1;
 		else if(n==2) return 1;
-		else return DFS(n-2)+DFS(n-1);
+		else return fibo[n] = DFS(n-2)+DFS(n-1);
 	}
 	public static void main(String[] args) {
 		S_0104 T = new S_0104();
 		int n = 10;
+		fibo = new int[n+1];
+		T.DFS(n);
 		for (int i = 0; i <= n ; i++) 
-			System.out.println(T.DFS(i) + " ");
+			System.out.println(fibo[i] + " ");
 	}
 }
