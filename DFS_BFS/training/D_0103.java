5a86c98d2fb2819a5f179de321648d07d169be7e
gayoung choi <cy614@naver.com>
2022-05-15 23:08:29 +0900

    최대 점수 구하기(DFS)

---
+++ DFS_BFS/training/D_0103.java
@@ -0,0 +1,34 @@
+package training;
+
+import java.util.Scanner;
+
+//최대 점수 구하기 
+public class D_0103 {
+	static int answer = Integer.MIN_VALUE, n, m; //최대값을 찾을때는 MIN_VALUE
+	boolean flag = false;
+	public void DFS(int L, int sum, int time, int[] ps, int[] pt) {
+		if(time>m) return;
+		if(L == n) {
+			answer = Math.max(answer, sum);
+		}
+		else {
+			DFS(L+1, sum+ps[L], time+pt[L], ps, pt);
+			DFS(L+1, sum, time, ps, pt);
+		}
+	}
+	
+	public static void main(String[] args) {
+		D_0103 T = new D_0103();
+		Scanner kb = new Scanner(System.in);
+		n = kb.nextInt();
+		m = kb.nextInt();
+		int[] a = new int[n];
+		int[] b = new int[n];
+		for(int i = 0; i<n; i++) {
+			a[i] = kb.nextInt();
+			b[i] = kb.nextInt();
+		}
+		T.DFS(0, 0, 0, a, b);
+		System.out.println(answer);
+	}
+}
