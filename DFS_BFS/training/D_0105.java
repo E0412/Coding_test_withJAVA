6aee133895ac51cebb38920f335c0f41463ff021
gayoung choi <cy614@naver.com>
2022-05-17 12:22:27 +0900

    동전 교환(DFS, 복잡도개선)

--- DFS_BFS/training/D_0105.java
+++ DFS_BFS/training/D_0105.java
@@ -1,11 +1,13 @@
 package training;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 //동전 교환 
 public class D_0105 {
 	static int n, m, answer = Integer.MAX_VALUE;
-	public void DFS(int L, int sum, int[] arr) {
+	public void DFS(int L, int sum, Integer[] arr) {
 		if(sum>m) return; //무한루프를 막는 코드 
 		if(sum == m) {
 			answer = Math.min(answer, L);
@@ -20,10 +22,9 @@ public class D_0105 {
 		D_0105 T = new D_0105();
 		Scanner kb = new Scanner(System.in);
 		n = kb.nextInt();
-		int[] arr = new int[n];
-		for (int i = 0; i < n; i++) {
-			arr[i] = kb.nextInt();
-		}
+		Integer[] arr = new Integer[n];
+		for(int i =0 ; i<n; i++) arr[i] = kb.nextInt();
+		Arrays.sort(arr, Collections.reverseOrder());//내림차순
 		m = kb.nextInt();
 		T.DFS(0, 0, arr);
 		System.out.println(answer);
