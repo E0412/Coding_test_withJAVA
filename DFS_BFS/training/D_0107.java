16fc9cc909e30e506bcdd858e8b973ab8c579c03
gayoung choi <cy614@naver.com>
2022-05-20 23:06:10 +0900

    조합수(메모이제이션, 출력개선)

--- DFS_BFS/training/D_0107.java
+++ DFS_BFS/training/D_0107.java
@@ -4,9 +4,11 @@ import java.util.Scanner;
 
 //조합수(메모이제이션)
 public class D_0107 {
+	int[][] dy = new int[35][35];
 	public int DFS(int n, int r) {
-		if(n==r || r == 0) return 1; //1을 return하는 경우 
-		else return DFS(n-1, r-1) + DFS(n-1, r); //조합의 공식
+		if(dy[n][r]>0) return dy[n][r];
+		if(n == r || r == 0) return 1; //1을 return하는 경우 
+		else return dy[n][r] = DFS(n-1, r-1) + DFS(n-1, r); //조합의 공식
 	}
 	
 	public static void main(String[] args) {
