84f72b904ef9763d08f3572f8356fc00a9d00b6f
gayoung choi <cy614@naver.com>
2022-06-04 21:07:41 +0900

    돌다리 건너기(DP)

---
+++ DP/D_0102.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+//돌다리 건너기(DP) 
+public class D_0102 {
+	static int[] dy;
+	public int solution(int n) {
+		dy[1] = 1;
+		dy[2] = 2;
+		for(int i = 3; i<=n+1; i++) {
+			dy[i] = dy[i-2]+dy[i-1];
+		}
+		return dy[n+1];
+	}
+	public static void main(String[] args) {
+		D_0102 T = new D_0102();
+		Scanner kb = new Scanner(System.in);
+		int n = kb.nextInt();
+		dy = new int[n+2]; //크기
+		System.out.println(T.solution(n));
+	}
+}
