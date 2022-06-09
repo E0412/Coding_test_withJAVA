9f83607441e12ab3d06a5877943bbdcbd8746453
gayoung choi <cy614@naver.com>
2022-06-09 11:44:22 +0900

    최대 점수 구하기(냅색 알고리즘)

---
+++ DP/D_0106.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+//최대 점수 구하기(냅색 알고리즘)
+public class D_0106 {
+	public static void main(String[] args) {
+		Scanner kb = new Scanner(System.in);
+		int n = kb.nextInt();
+		int m = kb.nextInt();
+		int[] dy = new int[m+1];
+		for(int i = 0; i<n; i++) {
+			int ps = kb.nextInt();
+			int pt = kb.nextInt();
+			for(int j = m; j>=pt; j--) {
+				dy[j]= Math.max(dy[j], dy[j-pt]+ps); 
+			}
+		}
+		System.out.println(dy[m]);
+	}
+}
