bc5c9e5d26039c7f0773d1b28a47ad7cd438d8c9
gayoung choi <cy614@naver.com>
2022-06-12 22:44:05 +0900

    아스키 코드

---
+++ Backjun/solution/B_11654.java
@@ -0,0 +1,13 @@
+package solution;
+
+import java.util.Scanner;
+
+//아스키코드
+public class B_11654 {
+	public static void main(String[] args) {
+		Scanner kb = new Scanner(System.in);
+		int ch = kb.next().charAt(0);
+		System.out.println(ch);
+	}
+}
+
