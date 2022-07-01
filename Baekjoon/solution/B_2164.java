ed328fe6f20b5d684b3d5b551c75bc51986a1433
gayoung choi <cy614@naver.com>
2022-07-01 13:00:59 +0900

    카드게임(Queue)

---
+++ Baekjoon/solution/B_2164.java
@@ -0,0 +1,24 @@
+package solution;
+
+import java.io.*;
+import java.util.*;
+
+//카드 게임 
+public class B_2164 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Queue<Integer> queue = new LinkedList<Integer>();
+		int N  = Integer.parseInt(br.readLine());
+		
+		//카드를 큐에 저장
+		for(int i = 1; i <= N; i++) {
+			queue.add(i);
+		}
+		//카드가 1장 남을때까지 반복
+		while(queue.size() > 1) {
+			queue.poll(); //맨 위의 카드를 버린다
+			queue.add(queue.poll()); //맨 위의 카드를 가장 아래 카드 밑으로
+		}
+		System.out.println(queue.poll());
+	}
+}
