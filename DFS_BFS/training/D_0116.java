ad509090d6d01915158f32ed8ab88f9861dbf81a
gayoung choi <cy614@naver.com>
2022-08-03 23:01:54 +0900

    연결 요소의 개수(DFS)

---
+++ DFS_BFS/training/D_0116.java
@@ -0,0 +1,61 @@
+package training;
+
+import java.io.*;
+import java.util.*;
+
+//연결 요소의 개수(DFS)
+public class D_0116 {
+	
+	static ArrayList<Integer>[] lists; //그래프 데이터 저장 인접리스트
+	static boolean visited[]; //방문 기록 저장 배열
+	
+	//DFS 구현
+	static void DFS(int V) {
+		//현재 노드 == 방문 노드
+		if(visited[V]) {
+			return;
+		}
+		visited[V] = true; //현재 노드 방문 저장 
+		for(int i : lists[V]) {
+			//연결 노드 중 방문하지 않은 노드만 탐색 
+			if(visited[i] == false) {
+				DFS(i);
+			}
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		lists = new ArrayList[N+1];
+		visited = new boolean[N+1];
+		
+		//인접 리스트 초기화 
+		for (int i = 1; i < N + 1; i++) {
+			lists[i] = new ArrayList<Integer>();
+		}
+		
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int start = Integer.parseInt(st.nextToken()); //간선 시작점
+			int end = Integer.parseInt(st.nextToken()); //간선 끝
+			
+			//양방향 간선이므로 양쪽에 간선 더하기 
+			lists[start].add(end);
+			lists[end].add(start);
+		}
+		int cnt = 0;
+		for (int i = 1; i < N + 1; i++) {
+			//방문하지 않은 노드가 없을때까지 반복 
+			if(!visited[i]) {
+				cnt++;
+				DFS(i);
+			}
+		}
+		System.out.println(cnt);
+	}
+}
