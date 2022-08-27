f9f6258fd1311ca6414fd2d66973bc99e1d986f5
gayoung choi <cy614@naver.com>
2022-08-28 00:11:44 +0900

    최대 공약수 구하기(수정)

--- Baekjoon/solution/B_1850.java
+++ Baekjoon/solution/B_1850.java
@@ -9,7 +9,7 @@ public class B_1850 {
 	public static long gcd(long a, long b) {
 		long r = 0;
 		while(b != 0) {
-			r = a * b;
+			r = a % b; //a * b로 잘못 입력하여 오류가 있었다
 			a = b;
 			b = r;
 		}
@@ -20,12 +20,14 @@ public class B_1850 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		long N = Long.parseLong(st.nextToken());
-		long M = Long.parseLong(st.nextToken());
+		
+		long N = Long.parseLong(st.nextToken()); //N의 길이 
+		long M = Long.parseLong(st.nextToken()); //M의 길이 
 		
 		long answer = gcd(N, M);
+		//최대공약수의 길이만큼 1을 반복하여 출력한다 
 		while(answer > 0) {
-			bw.write(1);
+			bw.write("1");
 			answer--;
 		}
 		bw.flush();
