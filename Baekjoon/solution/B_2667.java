5d83e773a54010be0ca29e382eaf0483708a548a
gayoung choi <cy614@naver.com>
2022-09-24 23:52:50 +0900

    단지 번호 붙이기(DFS) - 시도2

--- Baekjoon/solution/B_2667.java
+++ Baekjoon/solution/B_2667.java
@@ -4,13 +4,18 @@ import java.io.*;
 import java.util.*;
 
 //단지 번호 붙이기(DFS)
-//1은 집이 있는 곳을, 0은 집이 없는 곳을 의미한다 
+/**
+ * 1은 집이 있는 곳을, 0은 집이 없는 곳을 의미한다 
+ * 첫 번째 줄에는 총 단지수를 출력
+ * 각 단지내 집의 수를 오름차순으로 정렬하여 한 줄에 하나씩 출력
+ */
 public class B_2667 {
 	
 	public static int N; //입력
 	//2차원 배열 graph 
-	public static int graph[][];
-	public static int cnt;
+	public static int graph[][] = new int[25][25];
+	public static int total; //총 단지 수 
+	public static int cnt; //단지내 집의 수 
 	
 	//DFS로 특정 노드를 방문한 후 연결된 모든 노드를 방문 
 	public static boolean DFS(int x, int y) {
@@ -19,10 +24,16 @@ public class B_2667 {
 			return false;
 		}
 		
-		//현재 노드를 방문하지 않은 경우 
+		//상, 하, 좌, 우를 살펴본 후 값이 0이면서 방문하지 않은 지점 방문 
 		if(graph[x][y] == 0) {
 			graph[x][y] = 1; 
 			
+			//상, 하, 좌, 우의 위치들을 재귀적으로 호출
+			DFS(x - 1, y); //좌
+			DFS(x + 1, y); //우
+			DFS(x, y + 1); //상
+			DFS(x, y - 1); //하 
+			return true; //방문 처리 
 		}
 		return false;
 	}
@@ -33,9 +44,11 @@ public class B_2667 {
 		
 		N = Integer.parseInt(br.readLine());
 		
+		//2차원 리스트의 맵 입력받기
 		for (int i = 0; i < N; i++) {
 			String s = br.readLine();
 			for (int j = 0; j < N; j++) {
+				graph[i][j] = s.charAt(j) - '0'; //각 문자열을 확인한다 
 			}
 		}
 		
@@ -44,16 +57,13 @@ public class B_2667 {
 			for (int j = 0; j < N; j++) {
 				//현재 위치에서 DFS 실행
 				if(DFS(i, j)) {
-					cnt++;
+					total++; //방문하지 않은 단지 수++
 				}
 			}
 		}
-		//오름차순 정렬
-		for (int i = 1; i < graph.length; i++) {
-		}
-		
-		bw.write(cnt + "\n");
+		bw.write(total + "\n"); //총 단지수 
 		bw.flush();
 		bw.close();
 	}
 }
+
