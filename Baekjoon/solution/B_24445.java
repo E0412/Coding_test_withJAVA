07c4c91e801f788de3f048493e014b635a2fcf1f
gayoung choi <cy614@naver.com>
2022-10-06 23:22:05 +0900

    너비 우선 탐색 2 - 시도(2)

--- Baekjoon/solution/B_24445.java
+++ Baekjoon/solution/B_24445.java
@@ -3,46 +3,51 @@ package solution;
 import java.io.*;
 import java.util.*;
 
+import java.io.*;
+import java.util.*;
+
 //너비 우선 탐색 2 
-//무방향 그래프 구현, 1번부터 방문  
+//무방향 그래프 구현, 1번부터 방문 
+//노드의 방문 순서를 출력, 방문할 수 없는 경우 0 출력 
 public class B_24445 {
 
-	public static boolean visited[]; //방문 여부
+	public static int visited[]; //방문 여부
 	public static int V, E, start; //정점, 간선, 시작점
 	public static int[][] graph; //그래프 
 	public static int cnt = 1; //방문 순서 
-
+	//ArrayList로 2차원 배열로 바꿔보기
 	
-	public void BFS(int x) {
+	public static void BFS(int x) {
 		Queue<Integer> q = new LinkedList<Integer>();
-		visited[x] = true;
 		q.offer(x);
+		visited[x] = cnt; //방문한 정점에 순서 저장
 		
 		while(!q.isEmpty()) {
 			int y = q.poll();
-			System.out.print(y + " ");
+			cnt++;
+			visited[y] = cnt;
 			
-			for(int ny : graph[x]) {
+			for(int i = 1; i <= V; i++) {  
 			//해당 위치를 방문하지 않았을 때	
-			if(visited[ny] == false) {
-					visited[ny] = true;
-					q.offer(ny);
+			if(graph[x][i] == 1 && visited[i] == 0) {
+					q.offer(i);
+					visited[i] = cnt;
 			}
 			}
 		}
 	}
 	
 	public static void main(String[] args) throws IOException { 
-		B_24445 T = new B_24445();
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		
 		V = Integer.parseInt(st.nextToken());
 		E = Integer.parseInt(st.nextToken());
 		start = Integer.parseInt(st.nextToken());
 		
-		graph = new int[V][V];
-		visited = new boolean[V];
+		graph = new int[V + 1][V + 1];
+		visited = new int[V + 1];
 		
 		for (int i = 0; i < E; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
@@ -51,6 +56,13 @@ public class B_24445 {
 			
 			graph[a][b] = graph[b][a] = 1; 
 		}
-		T.BFS(1);
+		BFS(start);
+		
+		Arrays.sort(visited);
+		for (int i = 1; i <= V; i++) {
+			bw.write(visited[i] + "\n");
+		}
+		bw.flush();
+		bw.close();
 	}
 }
