9a7ea1fe613cb396a24ecef55e830799144f105e
gayoung choi <cy614@naver.com>
2022-10-08 00:37:15 +0900

    너비 우선 탐색 2(내림차순) - 해결

--- Baekjoon/solution/B_24445.java
+++ Baekjoon/solution/B_24445.java
@@ -9,67 +9,68 @@ import java.util.*;
 public class B_24445 {
 
 	public static boolean visited[]; //방문 여부
-	public static int save[]; //방문 여부
+	public static int save[]; //순서 저장 
+	public static int cnt = 1; //순서 1부터 시작
 	public static int V, E, start; //정점, 간선, 시작점
-	public static ArrayList<ArrayList<Integer>> graph; //그래프 
-	
+	public static ArrayList<ArrayList<Integer>> graph = new ArrayList<>(); //그래프 
 
 	//BFS 구현 
 	public static void BFS(int x) {
 		Queue<Integer> q = new LinkedList<Integer>();
-		q.offer(x);
+		save[x] = cnt++;
 		visited[x] = true; 
-		
-		
-		int cnt = 0; //방문 순서 
-		
+		q.offer(x);
+
 		while(!q.isEmpty()) {
 			int y = q.poll();
-			save[y] = cnt++; //순서 증가 
-			
-			for(int i = 0; i <= graph.get(y).size(); i++) {  
-			//해당 위치를 방문하지 않았을 때	
-			if(!visited[i]) {
-					visited[i] = true;
-					q.offer(i);
-			}
+
+			//내림차순 정렬
+			Collections.sort(graph.get(y), Collections.reverseOrder());
+
+			//정점 탐색 
+			for(int ny : graph.get(y)) {  
+				//해당 위치를 방문하지 않았을 때	
+				if(!visited[ny]) {
+					visited[ny] = true;
+					save[ny] = cnt++;
+					q.offer(ny);
+				}
 			}
 		}
 	}
-	
+
 	public static void main(String[] args) throws IOException { 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		
+
 		V = Integer.parseInt(st.nextToken());
 		E = Integer.parseInt(st.nextToken());
 		start = Integer.parseInt(st.nextToken());
-		
+
+		visited = new boolean[V + 1];
+		save = new int[V + 1];
+
 		//그래프 크기 할당 
 		for(int i = 0; i <= V; i++) {
 			graph.add(new ArrayList<Integer>());
 		}
-		
-		visited = new boolean[V + 1];
-		save = new int[V + 1];
-		
+
 		for (int i = 0; i < E; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
-			
+
 			//무방향 그래프 할당 
 			graph.get(a).add(b);
 			graph.get(b).add(a);
 		}
 
+		BFS(start);
+
+		//순번 저장 
 		for (int i = 1; i <= V; i++) {
-			Collections.sort(graph.get(i), Collections.reverseOrder());
-			BFS(start);
-		}
-		for (int i = 1; i <= V; i++) {
-			bw.write(save[i]);
+			bw.write(save[i] + "\n");
 		}
 		bw.flush();
 		bw.close();
