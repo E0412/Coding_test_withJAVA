0f521a37650086e9ed4da1e40d83806ddaf70a91
gayoung choi <cy614@naver.com>
2022-10-27 23:55:16 +0900

    feat : 나이트의 이동 - 시도(2)

--- Baekjoon/solution/B_7562.java
+++ Baekjoon/solution/B_7562.java
@@ -4,14 +4,39 @@ import java.io.*;
 import java.util.*;
 
 //나이트의 이동 
+/*
+ * 테스트 케이스, 체스판 한 변의 길이, 현재 나이트의 위치, 나이트의 이동 위치 입력
+ * 각 케이스마다 나이트가 최소 몇 번만에 이동할 수 있는지 출력  
+ */
 public class B_7562 {
 
-	public static int tc, N; 
+	//나이트의 이동 범위
+	static int dx[] = {-2, -1, 1, 2, 2, 1, -1, -2}; 
+	static int dy[] = {1, 2, 2, 1, -1, -2, -2, -1};
+
+	public static int tc, N, total; 
 	public static boolean[][] visited;
 	public static int[][] graph;
 
-	public static void DFS(int x) {
+	public static void BFS(int x, int y) {
+		Queue<int[]> q = new LinkedList<>();
+		q.offer(new int[] {x, y});
+		visited[x][y] = true;
 
+		while(!q.isEmpty()) {
+			int point[] = q.poll();
+
+			for (int i = 0; i < 8; i++) {
+				int nx = point[0] + dx[i];
+				int ny = point[1] + dy[i];
+
+				if(!visited[nx][ny] && graph[nx][ny] == 1) {
+					total++;
+					visited[nx][ny] = true;
+					q.offer(new int[] {nx, ny});
+				}
+			}
+		}
 	}
 
 	public static void main(String[] args) throws IOException{
