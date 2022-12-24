761a155cba80310dacb21c4863551abac93fc432
gayoung choi <cy614@naver.com>
2022-12-24 23:57:30 +0900

    feat : 경로 찾기 - 다른 풀이법

---
+++ Baekjoon/solution/B11403.java
@@ -0,0 +1,39 @@
+package solution;
+
+import java.io.*;
+import java.util.*;
+
+//경로 찾기
+/*
+ * 가중치 없는 방향 그래프 G가 주어졌을 때, 
+ * 모든 정점 (i, j)에 대해서, i에서 j로 가는 경로가 있는지 없는지 구하는 프로그램을 작성
+ * i번째 줄의 j번째 숫자가 1인 경우에는 i에서 j로 가는 간선이 존재, 0인 경우는 없다
+ */
+public class B11403 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+
+		int N = Integer.parseInt(br.readLine());
+		int[][] graph = new int[N][N];
+
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				graph[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				bw.write(graph[i][j] + " ");
+			}
+			bw.write("\n");
+		}
+		
+		bw.flush();
+		bw.close();
+	}
+}
