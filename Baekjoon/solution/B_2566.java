dfcd0e5be8f3afacc7e22dff9c9c7a893c465c2e
gayoung choi <cy614@naver.com>
2022-12-27 01:11:13 +0900

    feat : 최댓값(2차원 배열) - 해결

--- Baekjoon/solution/B_2566.java
+++ Baekjoon/solution/B_2566.java
@@ -14,26 +14,27 @@ public class B_2566 {
 		StringTokenizer st;
 
 		int[][] arr = new int[9][9];
-		
-		for (int i = 0; i < arr.length; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < arr.length; j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken()); 
-			}
-		}
-		
+
 		int max = 0;
 		int n = 0; int m = 0; //행과 열 
-		for (int i = 0; i < arr.length; i++) {
-			for (int j = 0; j < arr.length; j++) {
+
+		for (int i = 0; i < 9; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < 9; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken()); 
+
 				if(max < arr[i][j]) {
 					max = arr[i][j];
-					n = i; m = j; //행과 열 위치 
+					//행과 열 위치
+					n = i + 1; 
+					m = j + 1;  
 				}
 			}
 		}
 		bw.write(max + "\n");
 		//행과 열 위치 출력
-		bw.write((n + 1) + " " + (m + 1));
+		bw.write(n + " " + m);
+		bw.flush();
+		bw.close();
 	}
 }
