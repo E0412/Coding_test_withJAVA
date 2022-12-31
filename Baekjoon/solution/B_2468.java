0f2afda334bc569043acce4469d9fc77cf9eca83
gayoung choi <cy614@naver.com>
2022-12-31 22:24:28 +0900

    feat : 안전 영역(DFS), 시도(2)

--- Baekjoon/solution/B_2468.java
+++ Baekjoon/solution/B_2468.java
@@ -3,15 +3,35 @@ package solution;
 import java.io.*;
 import java.util.*;
 
-//안전 영역
+//안전 영역(DFS)
 /*
  * 어떤 지역의 높이 정보가 주어졌을 때, 장마철에 물에 잠기지 않는 안전한 영역의 최대 개수를 계산
- * 안전한 영역 == 물에 잠기지 않는 지점들이 위, 아래, 오른쪽 혹은 왼쪽으로 인접하고 그 크기가 최대인 영역
+ * 안전한 영역은 물에 잠기지 않는 지점들이 위, 아래, 오른쪽 혹은 왼쪽으로 인접하고 그 크기가 최대인 영역
  */
 public class B_2468 {
 
 	static int N; 
-	static int[][] graph;
+	static int[][] graph; //2차원 배열 지도
+	static int[][] map; //변형 된 지도 
+	static boolean[][] visited;
+	static int dx[] = {1, 0, -1, 0};
+	static int dy[] = {0, -1, 0, 1};
+	
+	
+	static void DFS(int x, int y) {
+		visited[x][y] = true;
+		
+		for (int i = 0; i < 4; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+			
+			if(nx >= 0 && ny >= 0 && nx < N && ny < N) {
+				if(!visited[nx][ny] && map[nx][ny] == 1) {
+					DFS(nx, ny);
+				}
+			}
+		}
+	}
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
