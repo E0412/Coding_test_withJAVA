58fa2c5fa29f3bd408a3af1e00c2ab9a323b977d
gayoung choi <cy614@naver.com>
2023-01-01 23:36:10 +0900

    feat : 안전 영역(DFS), 시도(3)

--- Baekjoon/solution/B_2468.java
+++ Baekjoon/solution/B_2468.java
@@ -16,15 +16,15 @@ public class B_2468 {
 	static boolean[][] visited;
 	static int dx[] = {1, 0, -1, 0};
 	static int dy[] = {0, -1, 0, 1};
-	
-	
+
+
 	static void DFS(int x, int y) {
 		visited[x][y] = true;
-		
+
 		for (int i = 0; i < 4; i++) {
 			int nx = x + dx[i];
 			int ny = y + dy[i];
-			
+
 			if(nx >= 0 && ny >= 0 && nx < N && ny < N) {
 				if(!visited[nx][ny] && map[nx][ny] == 1) {
 					DFS(nx, ny);
@@ -32,22 +32,52 @@ public class B_2468 {
 			}
 		}
 	}
-	
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st;
-		
+
 		int N = Integer.parseInt(br.readLine());
 		graph = new int[N][N];
-		
+		visited = new boolean[N][N];
+
+		int max = 1; //최댓값
+		int cnt = 0; //안전 지역 개수 
+
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < N; j++) {
 				graph[i][j] = Integer.parseInt(st.nextToken()); 
 			}
 		}
-		
+
+		for (int h = 1; h <= 100; h++) {
+			map = new int[N][N];
+
+			for (int n = 0; n < N; n++) {
+				for (int m = 0; m < N; m++) {
+					//높이 이하면 0으로 저장
+					if(map[n][m] <= h) {
+						map[n][m] = 0; 
+					} 
+					//아닌경우 1로 저장 
+					else map[n][m] = 1; 
+				}
+			}
+			
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					if(graph[i][j] == 1 && !visited[i][j]) {
+						DFS(i, j);
+						cnt++;
+					}
+				}
+			}
+			max = Math.max(max, cnt); //둘 중 큰 수를 저장 
+		}
+
+		bw.write(max + "");
 		bw.flush();
 		bw.close();
 	}
