2d00a0667766ec6e34ef581a2f7d1015aad65d10
gayoung choi <cy614@naver.com>
2023-01-07 23:50:34 +0900

    feat : 단어 뒤집기 2(stack), 시도(2)

--- Baekjoon/solution/B_17413.java
+++ Baekjoon/solution/B_17413.java
@@ -14,11 +14,46 @@ public class B_17413 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		
+
 		String S = st.nextToken(); //문자열 입력
-		String result = "";
-		Stack<String> s = new Stack<>(); 
-		
+		Stack<Character> stack = new Stack<>(); 
+
+		boolean check = false; //괄호 확인 
+
+		for (int i = 0; i < S.length(); i++) {
+			//여는 괄호의 경우 
+			if(S.charAt(i) == '<') {
+				check = true;
+
+				while(!stack.isEmpty()) {
+					bw.write(stack.pop()); 
+				}
+				bw.write(S.charAt(i));
+			}
+			//닫는 괄호인 경우
+			else if(S.charAt(i) == '>') {
+				check = false;
+				bw.write(S.charAt(i)); //그대로 출력 
+			}
+			else if(check) {
+				bw.write(S.charAt(i));
+			}
+			//괄호 밖에 있는 경우 
+			else {
+				//공백일 때 전부 pop 한다 
+				if(S.charAt(i) == ' ') {
+					while(!stack.isEmpty()) {
+						bw.write(stack.pop());
+					}
+					bw.write(S.charAt(i));
+				} else {
+					stack.push(S.charAt(i));  //스택에 추가 
+				}
+			}
+		}
+		while(!stack.isEmpty()) {
+			bw.write(stack.pop());
+		}
 		bw.flush();
 		bw.close();
 	}
