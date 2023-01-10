eb071581d9e0d526e14ad85a117f8bc7c5601c39
gayoung choi <cy614@naver.com>
2023-01-10 23:33:38 +0900

    feat : 단어 뒤집기 2(stack), 해결

--- Baekjoon/solution/B_17413.java
+++ Baekjoon/solution/B_17413.java
@@ -7,53 +7,57 @@ import java.util.*;
 /*
  * 태그는 '<'로 시작해서 '>'로 끝나는 길이가 3 이상인 부분 문자열 
  * '<'와 '>' 사이에는 알파벳 소문자와 공백만 있다.
- * 태그를 제외한 문자열 S의 단어를 뒤집어서 출력
+ * 태그를 제외한 문자열 S의 단어를 뒤집어서 출력 -> 스택에 추가 후 출력 
  */
 public class B_17413 {
+	
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	
+	static void out(Stack<Character> s) throws IOException {
+		while(!s.isEmpty()) {
+			bw.write(s.pop().toString());
+		}
+	}
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-		String S = st.nextToken(); //문자열 입력
+		String S = br.readLine(); //문자열 입력
 		Stack<Character> stack = new Stack<>(); 
 
 		boolean check = false; //괄호 확인 
 
 		for (int i = 0; i < S.length(); i++) {
-			//여는 괄호의 경우 
+			//여는 괄호, 태그 시작  
 			if(S.charAt(i) == '<') {
 				check = true;
 
-				while(!stack.isEmpty()) {
-					bw.write(stack.pop()); 
-				}
+				//태그 전까지의 단어를 뒤집는다 
+				out(stack);
 				bw.write(S.charAt(i));
 			}
-			//닫는 괄호인 경우
+			//닫는 괄호인 경우, 태그 끝부분
 			else if(S.charAt(i) == '>') {
 				check = false;
 				bw.write(S.charAt(i)); //그대로 출력 
 			}
+			//태그 안에 있는 경우 그대로 출력
 			else if(check) {
 				bw.write(S.charAt(i));
 			}
-			//괄호 밖에 있는 경우 
-			else if(!check) {
+			
+			//태그 밖에 있는 경우 
+			else {
 				//공백일 때 전부 pop 한다 
 				if(S.charAt(i) == ' ') {
-					while(!stack.isEmpty()) {
-						bw.write(stack.pop());
-					}
+					out(stack);
 					bw.write(S.charAt(i));
 				} else {
 					stack.push(S.charAt(i));  //스택에 추가 
 				}
 			}
 		}
-		while(!stack.isEmpty()) {
-			bw.write(stack.pop());
-		}
+		out(stack);
 		bw.flush();
 		bw.close();
 	}
