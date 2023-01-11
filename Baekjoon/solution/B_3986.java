5229a24dea2d7612bb4d192e91da2bcd8c1b2719
gayoung choi <cy614@naver.com>
2023-01-11 23:13:44 +0900

    feat : 좋은 단어(stack), 시도(1)

---
+++ Baekjoon/solution/B_3986.java
@@ -0,0 +1,28 @@
+package solution;
+
+import java.io.*;
+import java.util.*;
+
+//좋은 단어(stack)
+/*
+ * 좋은 단어란 단어 위로 아치형 곡선을 그어 글자끼리 쌍을 지을 때 
+ * 선이 교차하지 않으면서 짝을 지을 수 있는 단어이다
+ * 좋은 단어의 개수를 출력한다 
+ */
+public class B_3986 {
+	public static void main(String[] args)  throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int N = Integer.parseInt(br.readLine());
+		Stack<String> s = new Stack<>();
+		
+		for (int i = 0; i < N; i++) {
+			String in = br.readLine();
+			s.push(in);
+		}
+		
+		bw.flush();
+		bw.close();
+	}
+}
