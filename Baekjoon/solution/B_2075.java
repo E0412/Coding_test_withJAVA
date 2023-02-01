b130976208f185b2ed346a4bf9951fc663de3679
gayoung choi <cy614@naver.com>
2023-02-01 22:24:53 +0900

    feat : N번째 큰 수, priority queue

--- Baekjoon/solution/B_2075.java
+++ Baekjoon/solution/B_2075.java
@@ -11,17 +11,26 @@ public class B_2075 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
+
 		int N = Integer.parseInt(br.readLine());
 		//Queue의 구조를 가지며, 들어온 순서가 아닌 우선순위를 먼저 결정하고 내보낸다
 		PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
-		
+
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
-			int m = Integer.parseInt(st.nextToken());
-			pq.offer(m);
+			int a = Integer.parseInt(st.nextToken());
+			pq.offer(a);
 		}
-		
+
+		for (int i = 1; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				int b = Integer.parseInt(st.nextToken());
+				pq.offer(b);
+				pq.poll();
+			}
+		}
+		bw.write(pq.poll() + "");
 		bw.flush();
 		bw.close();
 	}
