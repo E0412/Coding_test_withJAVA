963872f4159d9f7b08a475664351594f987f6077
gayoung choi <cy614@naver.com>
2023-02-06 23:46:21 +0900

    feat : 국회의원 선거(PriorityQueue), 시도(1)

--- Baekjoon/solution/B_1417.java
+++ Baekjoon/solution/B_1417.java
@@ -1,9 +1,9 @@
 package solution;
 
 import java.io.*;
-import java.util.Arrays;
+import java.util.*;
 
-//국회의원 선거
+//국회의원 선거(PriorityQueue)
 /*  
  * 득표 수를 확인 후 매수해야 하는 사람의 최솟값을 출력
  */
@@ -13,14 +13,13 @@ public class B_1417 {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		int N = Integer.parseInt(br.readLine()); //후보의 수 
-
-
 		int som = Integer.parseInt(br.readLine()); //다솜이의 득표수
-		int[] arr = new int[N - 1];
+
+		PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
 
 		//다른 후보의 득표수 
 		for (int i = 0; i < N - 1; i++) 
-			arr[i] = Integer.parseInt(br.readLine());
+			pq.add(Integer.parseInt(br.readLine()));
 
 		int cnt = 0; //매수해야 하는 사람 
 		//후보자가 1명인 경우 0 출력
@@ -29,12 +28,10 @@ public class B_1417 {
 			return;
 		}
 
-		while(true) {
-			Arrays.sort(arr); //정렬
-			if(arr[N - 2] < som) break;
+		while(!pq.isEmpty() && pq.peek() >= som) {
 			cnt++;
-			arr[N - 2]--;
 			som++;
+			pq.add(pq.poll() - 1);
 		}
 		bw.write(cnt + "");
 		bw.flush();
