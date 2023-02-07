5367f99105def2fc3f11a4ab4d84b624487cd7b4
gayoung choi <cy614@naver.com>
2023-02-07 23:00:57 +0900

    feat : 국회의원 선거(PriorityQueue), 해결

--- Baekjoon/solution/B_1417.java
+++ Baekjoon/solution/B_1417.java
@@ -15,22 +15,19 @@ public class B_1417 {
 		int N = Integer.parseInt(br.readLine()); //후보의 수 
 		int som = Integer.parseInt(br.readLine()); //다솜이의 득표수
 
-		PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+		//Collections.reverseOrder() > 숫자의 우선순위 변경 
+		PriorityQueue<Integer> pq = new PriorityQueue<Integer>(Collections.reverseOrder());
 
 		//다른 후보의 득표수 
-		for (int i = 0; i < N - 1; i++) 
+		for (int i = 1; i < N; i++) 
 			pq.add(Integer.parseInt(br.readLine()));
 
 		int cnt = 0; //매수해야 하는 사람 
-		//후보자가 1명인 경우 0 출력
-		if(N == 1) {
-			bw.write(0 + ""); bw.flush(); 
-			return;
-		}
 
+		//priority queue에 할당된 득표수가 다솜이의 득표수보다 많은경우
 		while(!pq.isEmpty() && pq.peek() >= som) {
-			cnt++;
-			som++;
+			cnt++; //매수해야하는 사람 증다
+			som++; //다솜이의 득표수 증가
 			pq.add(pq.poll() - 1);
 		}
 		bw.write(cnt + "");
