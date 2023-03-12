b5a347d38773c454b235e36c4fcb6bca1f79eee9
gayoung choi <cy614@naver.com>
2023-03-12 23:51:24 +0900

    feat : 모음(1)

---
+++ Baekjoon/solution/B_1264.java
@@ -0,0 +1,20 @@
+package solution;
+
+import java.io.*;
+
+//모음
+/*
+ * 모음의 개수를 세는 프로그램 작성
+ * 모음 : 'a', 'e', 'i', 'o', 'u' 
+ */
+public class B_1264 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		String input = br.readLine();
+		
+		bw.flush();
+		bw.close();
+	}
+}
