dcb3b88bc4dd1506b49dc8499abfd78188ed23f9
gayoung choi <cy614@naver.com>
2023-03-13 23:54:38 +0900

    feat : 모음(구현, 해결)

--- Baekjoon/solution/B_1264.java
+++ Baekjoon/solution/B_1264.java
@@ -12,8 +12,29 @@ public class B_1264 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		String input = br.readLine();
-		
+		while(true) {
+			String input = br.readLine();
+			int cnt = 0;
+
+			if(input.equals("#")) {
+				break;
+			}
+
+			char[] arr = {'a', 'e', 'i', 'o', 'u'}; //모음을 가진 배열 
+			input = input.toLowerCase(); //입력받은 단어를 소문자로 변경
+
+			for (int i = 0; i < input.length(); i++) {
+				char ch = input.charAt(i);
+				for (int j = 0; j < arr.length; j++) {
+					//입력받은 값이 모음이면
+					if(ch == arr[j]) {
+						cnt++; //개수++
+					}
+				}
+			}
+			bw.write(cnt + "");
+			bw.write("\n");
+		}
 		bw.flush();
 		bw.close();
 	}
