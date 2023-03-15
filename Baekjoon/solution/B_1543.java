23434eb6c7d90dbb3cdf8647eede5a12b0e96845
gayoung choi <cy614@naver.com>
2023-03-15 23:20:26 +0900

    feat : 문서 검색(문자열, 해결)

--- Baekjoon/solution/B_1543.java
+++ Baekjoon/solution/B_1543.java
@@ -1,7 +1,7 @@
 package solution;
 
 import java.io.*;
-import java.util.StringTokenizer;
+
 
 //문서 검색 
 /*
@@ -11,11 +11,21 @@ public class B_1543 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st  = new StringTokenizer(br.readLine());	
-		
+
 		String doc = br.readLine();
 		String search = br.readLine();
-		
+
+		//문자열의 길이를 잰다
+		int len = doc.length(); //문서의 길이 
+		int size = search.length(); //단어의 길이
+
+		//replace 함수를 사용해 중복된 단어를 지운다
+		doc = doc.replace(search, "");
+
+		//지운 단어의 길이만큼 나눈다
+		int answer = (len - doc.length()) / size;
+
+		bw.write(answer + "");
 		bw.flush();
 		bw.close();
 	}
