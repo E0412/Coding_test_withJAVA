ea86f1db5346934c9b9943c05bf95999ee86ee6d
gayoung choi <cy614@naver.com>
2023-03-20 21:30:39 +0900

    feat : 공 바꾸기(1차원 배열), 해결

--- Baekjoon/solution/B_10813.java
+++ Baekjoon/solution/B_10813.java
@@ -13,14 +13,25 @@ public class B_10813 {
 		int N = Integer.parseInt(st.nextToken()); //바구니 개수 
 		int M = Integer.parseInt(st.nextToken()); //공을 바꾸는 횟수 
 
-		int arr[] = new int[N];
+		int arr[] = new int[N + 1];
+
+		for (int i = 1; i <= N; i++) {
+			arr[i] = i;
+		}
 
 		for (int i = 0; i < M; i++) {
 			st  = new StringTokenizer(br.readLine());
 			int I = Integer.parseInt(st.nextToken());
 			int J = Integer.parseInt(st.nextToken());
-		}
 
+			//배열의 값 교환
+			int tmp = arr[I];
+			arr[I] = arr[J];
+			arr[J] = tmp; 
+		}
+		for (int i = 1; i <= N; i++) {
+			bw.write(arr[i] + " ");
+		}
 		bw.flush();
 		bw.close();
 	}
