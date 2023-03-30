37b43661130857469a617c4b1c4246d6d811bebc
gayoung choi <cy614@naver.com>
2023-03-30 22:31:02 +0900

    feat : 막대기, 해결

--- Baekjoon/solution/B_17608.java
+++ Baekjoon/solution/B_17608.java
@@ -12,15 +12,24 @@ public class B_17608 {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		int N = Integer.parseInt(br.readLine()); //막대기 개수 
+
 		int[] arr = new int[N];
-		
-		int cnt = 0; //맨 나중에 입력한 값보다 크면 증가
+		int cnt = 1; //맨 나중에 입력한 값보다 크면 증가
+
 		for (int i = 0; i < N; i++) {
-			int input = Integer.parseInt(br.readLine());
-			
-			
+			arr[i] = Integer.parseInt(br.readLine());
 		}
-		
+
+		int max = arr[N - 1];
+		for (int i = N - 2; i >= 0; i--) {
+			//배열의 이전 값 < 현재 값 일 때 
+			if(max < arr[i]) {
+				max = arr[i]; //값 변경 
+				cnt++; //개수 증가 
+			}
+		}	
+
+		bw.write(cnt + "");
 		bw.flush();
 		bw.close();
 	}
