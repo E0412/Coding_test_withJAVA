17491211383a3d0bd924dfa96a6d86a10ca34b3f
gayoung choi <cy614@naver.com>
2023-04-01 23:42:46 +0900

    feat  : 보물(배열), 해결

--- Baekjoon/solution/B_1026.java
+++ Baekjoon/solution/B_1026.java
@@ -12,22 +12,31 @@ public class B_1026 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	
+
 		int N = Integer.parseInt(br.readLine());
-		
+
 		int[] A = new int[N];
 		int[] B = new int[N];
-		
+
 		StringTokenizer st  = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
 			A[i] = Integer.parseInt(st.nextToken());
 		}
+		Arrays.sort(A); //오름차순 정렬
 
 		st  = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
 			B[i] = Integer.parseInt(st.nextToken());
 		}
-		
+		Arrays.sort(B); 
+
+		int answer = 0;
+		//A배열의 최소값과 B배열의 최대값을 곱한 후 더한다 
+		for (int i = 0; i < N; i++) {
+			answer += A[i] * B[N - 1 - i]; //B[N - 1]이 최대값
+		}
+
+		bw.write(answer + "");
 		bw.flush();
 		bw.close();
 	}
