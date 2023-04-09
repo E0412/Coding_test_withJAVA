be479b6db00374e95ec0e91e48fbc179a759fe27
gayoung choi <cy614@naver.com>
2023-04-09 17:59:26 +0900

    feat : 적어도 대부분의 배수, 해결

--- Baekjoon/solution/B_1145.java
+++ Baekjoon/solution/B_1145.java
@@ -14,16 +14,31 @@ public class B_1145 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
+
 		int[] arr = new int[5];
-		
+
 		for (int i = 0; i < 5; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
-		
+
 		int answer = 1; //나누어 떨어지는 숫자가 3개이상 존재하는 값
 		int cnt = 0; //3개이상 나누어 떨어지는지 확인 
-		
+
+		while(true) {
+			for (int i = 0; i < 5; i++) {
+				//나누어 떨어지는 경우
+				if(answer % arr[i] == 0) {
+					cnt++; 
+				}
+			}
+			//3개 이상인 경우 answer 반환
+			if(cnt >= 3) {
+				bw.write(answer + "");
+				break;
+			}
+			cnt = 0;
+			answer++;
+		}
 		bw.flush();
 		bw.close();
 	}
