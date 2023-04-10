72ee4ad892921692b9e7cb2e834dde2f8a2507ac
gayoung choi <cy614@naver.com>
2023-04-10 19:59:16 +0900

    feat : 저항(ArrayList)

---
+++ Baekjoon/solution/B_1076.java
@@ -0,0 +1,40 @@
+package solution;
+
+import java.io.*;
+import java.util.ArrayList;
+
+//저항(ArrayList)
+/*
+ * 저항은 색 3개를 이용해서 그 저항이 몇 옴인지 나타낸다
+ * 음 색 2개는 저항의 값이고, 마지막 색은 곱해야 하는 값이다
+ */
+public class B_1076 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		ArrayList<String> list = new ArrayList<>();
+		
+		//리스트에 색상 목록을 추가한다 
+		list.add("black");
+		list.add("brown");
+		list.add("red");
+		list.add("orange");
+		list.add("yellow");
+		list.add("green");
+		list.add("blue");
+		list.add("violet");
+		list.add("grey");
+		list.add("white");
+		
+		//색을 입력받는다 
+		int c1 = list.indexOf(br.read()) * 10; //c1은 c2에 더해야하기 때문에 십의 자리로 만든다
+		int c2 = list.indexOf(br.read());
+		int c3 = list.indexOf(br.read());
+		
+		//저항의 값 = (c1 + c2) * c3 
+		
+		bw.flush();
+		bw.close();
+	}
+}
