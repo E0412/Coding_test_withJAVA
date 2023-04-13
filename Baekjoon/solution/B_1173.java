48f46c01ac6e335453c2f6b5ffa39f04f5aa1537
gayoung choi <cy614@naver.com>
2023-04-13 23:51:03 +0900

    feat : 운동(시뮬레이션), 해결

--- Baekjoon/solution/B_1173.java
+++ Baekjoon/solution/B_1173.java
@@ -20,8 +20,39 @@ public class B_1173 {
 		int T = Integer.parseInt(st.nextToken()); //맥박 증가치 
 		int R = Integer.parseInt(st.nextToken()); //맥박 감소치(휴식)
 
-		//운동 가능 X + T <= M 
-		
+		int time = 0; //최소 시간
+		int start = m; //초기 맥박
+		int ex = 0; //운동
+
+
+		while(ex != N) {
+			time++; //시간 증가
+
+			//운동 후 맥박이 최대 맥박보다 작을 시 운동시작
+			if(m + T <= M) {
+				m += T; //초기 맥박에 증가치를 더한다
+				ex++;
+			} 
+			//휴식
+			else {
+				m -= R; //초기 맥박에 감소치를 뺀다
+
+				//초기 맥박 이하인 경우 초기값으로 세팅
+				if(m < start)
+					m = start;
+			}
+			//운동 후 맥박이 최대보다 크고, 초기 맥박인 경우 중지
+			if((m + T > M) && (m == start)) 
+				break;
+		}
+
+		//운동을 N분 할 수 없다면 -1을 출력
+		if(ex != N) {
+			bw.write("-1");
+		} else {
+			bw.write(time + "");
+		}
+
 		bw.flush();
 		bw.close();
 	}
