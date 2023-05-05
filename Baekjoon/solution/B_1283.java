1d2df6caa0d654a39ab97c56ad81c20a5660691c
gayoung choi <cy614@naver.com>
2023-05-05 23:26:38 +0900

    feat : 단축키 지정(문자열), 해결

--- Baekjoon/solution/B_1283.java
+++ Baekjoon/solution/B_1283.java
@@ -2,7 +2,6 @@ package solution;
 
 import java.io.*;
 import java.util.HashMap;
-import java.util.Iterator;
 
 //단축키 지정
 public class B_1283 {
@@ -11,48 +10,48 @@ public class B_1283 {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		int N = Integer.parseInt(br.readLine());
-		String s = new String();
 		HashMap<Character, Integer> map = new HashMap<>();
 
+
 		for (int i = 0; i < N; i++) {
-			s = br.readLine(); 
-		}
+			String s = br.readLine(); 
 
-		String[] str = s.split(" "); //여러 단어로 이루어진 문자열
-		boolean check = true; //단축키 중복 체크 
+			StringBuilder sb = new StringBuilder();
+			String[] str = s.split(" "); //여러 단어로 이루어진 문자열
+			boolean check = true; //단축키 중복 체크 
 
-		for (String in : str) {
-			char ch = in.charAt(0);
-			char low = Character.toLowerCase(ch); //소문자로 변환
-
-			//단축키가 없는 경우
-			if(check && map.getOrDefault(low, 0) == 0 && low != ' ') {
-				map.put(low, 1);
-				bw.write("[" + ch + "]" + in.substring(in.indexOf(ch) + 1));
-				check = false; //중복처리
-			}
-			else {
-				bw.write(in + " ");
-			}
-		}
-		
-		if(!check) {
-			bw.write("\n");
-		}
-		else {
-			String out = "";
-			for (int i = 0; i < s.length(); i++) {
-				char ch = s.charAt(i);
+			for (String in : str) {
+				char ch = in.charAt(0);
 				char low = Character.toLowerCase(ch); //소문자로 변환
 
 				//단축키가 없는 경우
 				if(check && map.getOrDefault(low, 0) == 0 && low != ' ') {
 					map.put(low, 1);
-					out += "[" + ch + "]";
+					sb.append("[" + ch + "]" + in.substring(in.indexOf(ch) + 1) + " ");
 					check = false; //중복처리
 				}
 				else {
-					out += ch;
+					sb.append(in + " ");
+				}
+			}
+			if(!check) {
+				bw.write(sb + "\n");
+			}
+			else {
+				String out = "";
+				for (int j = 0; j < s.length(); j++) {
+					char ch = s.charAt(j);
+					char low = Character.toLowerCase(ch); //소문자로 변환
+
+					//단축키가 없는 경우
+					if(check && map.getOrDefault(low, 0) == 0 && low != ' ') {
+						map.put(low, 1);
+						out += "[" + ch + "]";
+						check = false; //중복처리
+					}
+					else {
+						out += ch;
+					}
 				}
 				bw.write(out + "\n");
 			}
