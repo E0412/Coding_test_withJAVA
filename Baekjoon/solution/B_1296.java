ffec9e8b0162c5e331a2538e002fc1f2abb87c55
gayoung choi <cy614@naver.com>
2023-05-08 22:29:45 +0900

    feat : 팀 이름 정하기(문자열)

--- Baekjoon/solution/B_1296.java
+++ Baekjoon/solution/B_1296.java
@@ -10,17 +10,33 @@ public class B_1296 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	
+
 		String name = br.readLine(); //연두의 영어이름
+
 		int N = Integer.parseInt(br.readLine()); //팀 이름 후보의 개수
-		
-		int max = 0;
-		
+		int arr[] = new int[4]; 
+
+		String love = "LOVE";
+		String non = "";
+		int max = -1;
+
 		for (int i = 0; i < N; i++) {
 			String team = br.readLine();
-			
+
+			for (int j = 0; j < 4; j++) {
+				int cnt = name.length() - name.replace(String.valueOf(love.charAt(j)), "").length();
+				cnt += team.length() - team.replace(String.valueOf(team.charAt(j)), "").length();
+				arr[j] = cnt; //값 저장
+			}
+
+			int ans = 1;
+			for (int j = 0; j < 4; j++) {
+				for (int k = j + 1; k < 4; k++) {
+					ans *= arr[j] + arr[k];
+				}
+			}
 		}
-		
+
 		bw.flush();
 		bw.close();
 	}
