ef62d34b876f5993ff898f197cc5b6db1c71f0e8
gayoung choi <cy614@naver.com>
2023-05-09 23:15:56 +0900

    feat : 팀 이름 정하기(문자열), 해결

--- Baekjoon/solution/B_1296.java
+++ Baekjoon/solution/B_1296.java
@@ -1,6 +1,7 @@
 package solution;
 
 import java.io.*;
+import java.util.Arrays;
 
 //팀 이름 정하기
 /*
@@ -14,29 +15,44 @@ public class B_1296 {
 		String name = br.readLine(); //연두의 영어이름
 
 		int N = Integer.parseInt(br.readLine()); //팀 이름 후보의 개수
+
 		int arr[] = new int[4]; 
+		int max = -1;
 
 		String love = "LOVE";
-		String non = "";
-		int max = -1;
+		String ans = ""; //답을 저장할 빈 문자열
 
 		for (int i = 0; i < N; i++) {
 			String team = br.readLine();
 
 			for (int j = 0; j < 4; j++) {
 				int cnt = name.length() - name.replace(String.valueOf(love.charAt(j)), "").length();
-				cnt += team.length() - team.replace(String.valueOf(team.charAt(j)), "").length();
+				cnt += team.length() - team.replace(String.valueOf(love.charAt(j)), "").length();
 				arr[j] = cnt; //값 저장
 			}
 
-			int ans = 1;
+			int sum = 1;
 			for (int j = 0; j < 4; j++) {
 				for (int k = j + 1; k < 4; k++) {
-					ans *= arr[j] + arr[k];
+					sum *= arr[j] + arr[k];
 				}
 			}
-		}
 
+			if(max == sum % 100 && ans != "") {
+				String[] s = new String[2]; //단어를 저장하는 배열
+				s[0] = ans; 
+				s[1] = team; //팀명 저장
+				//순서대로 정렬
+				Arrays.sort(s);
+				ans = s[0];
+			}
+
+			if(max < (sum % 100)) {
+				max = sum % 100;
+				ans = team;
+			}
+		}
+		bw.write(ans);
 		bw.flush();
 		bw.close();
 	}
