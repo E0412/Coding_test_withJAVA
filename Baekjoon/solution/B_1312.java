252ab52b825493fcf6b384de9921801be0212bdd
gayoung choi <cy614@naver.com>
2023-05-15 22:49:16 +0900

    feat : 소수(수학), 해결

--- Baekjoon/solution/B_1312.java
+++ Baekjoon/solution/B_1312.java
@@ -15,7 +15,16 @@ public class B_1312 {
 		int N = Integer.parseInt(st.nextToken()); //소수점 아래 N자리 수
 
 		//A / B를 했을 때 소수점 N번째 수 출력 
+		int num = A % B; //나머지
 
+		for (int i = 0; i < N - 1; i++) {
+			num *= 10; //나머지에 10을 곱한다
+			num %= B; //나머지를 구한다 
+		}
+
+		num *= 10; //구한  나머지에 10을 곱한다 
+
+		bw.write(num / B + ""); //원하는 자리수에서 나누기를 하여 몫을 구한다
 		bw.flush();
 		bw.close();
 	}
