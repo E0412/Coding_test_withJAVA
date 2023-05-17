168ad0a4a85169b2db0d39ed3316b53021bf8ee6
gayoung choi <cy614@naver.com>
2023-05-17 22:33:32 +0900

    feat : 부재중 전화(구현), 해결

--- Baekjoon/solution/B_1333.java
+++ Baekjoon/solution/B_1333.java
@@ -14,7 +14,25 @@ public class B_1333 {
 		int L = Integer.parseInt(st.nextToken()); //모든 노래의 길이
 		int D = Integer.parseInt(st.nextToken()); //전화벨이 울리는 간격
 
-		//전화가 오면 +1초 노래와 노래 사이에는 5초의 간격(개수당 5초 추가)
+		//전화가 오면 +1초 
+		int time[] = new int[10000];
+		int sec = 0;
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < L; j++) {
+				time[sec++] = 1;
+			}
+			sec += 5; //노래와 노래 사이에는 5초의 간격이 있다
+		}
+		sec = 0;
+
+		while(true) {
+			if(time[sec] == 0) {
+				bw.write(sec + "");
+				break;
+			}
+			sec += D; //간격을 초에 추가
+		}
 
 		bw.flush();
 		bw.close();
