1734cd8432ebd9bc19463e2d32b25f646b084a14
gayoung choi <cy614@naver.com>
2023-05-24 21:04:54 +0900

    feat : 유진수(문자열, 수학), 해결

--- Baekjoon/solution/B_1356.java
+++ Baekjoon/solution/B_1356.java
@@ -8,19 +8,35 @@ import java.io.*;
  * 앞부분 자리수의 곱과 뒷부분 자리수의 곱이 같을 때 유진수라고 한다.
  */
 public class B_1356 {	
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		int N = Integer.parseInt(br.readLine());
+		String input = br.readLine();
+		String result = "NO"; //결과값 디폴트 NO
 
-		if(true) {
-			bw.write("YES");
-		} else {
-			bw.write("NO");
+		//각 부분에 적어도 한자리가 있어야 함
+		if(input.length() > 1) {
+			for (int i = 1; i < input.length(); i++) {
+				int n = 1; //앞부분 자리수 곱
+				int m = 1; //뒷부분 자리수 곱
+
+				for (int j = 0; j < i; j++) {
+					n *= (input.charAt(j) - '0');
+				}
+
+				for (int k = i; k < input.length(); k++) {
+					m *= (input.charAt(k) - '0');
+				}
+
+				//두 자리수의 곱이 같을 때 유진수가 된다
+				if(n == m) {
+					result = "YES";
+					break;
+				}
+			}
 		}
-
+		bw.write(result);
 		bw.flush();
 		bw.close();
 	}
