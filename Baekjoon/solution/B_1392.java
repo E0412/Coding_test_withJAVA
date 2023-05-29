ebae33197a35ce96f1d0bae39d6edda2b78e85cb
gayoung choi <cy614@naver.com>
2023-05-29 23:56:21 +0900

    feat : 노래 악보(구현), 해결

--- Baekjoon/solution/B_1392.java
+++ Baekjoon/solution/B_1392.java
@@ -1,7 +1,7 @@
 package solution;
 
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 //노래 악보
 public class B_1392 {
@@ -13,12 +13,24 @@ public class B_1392 {
 		int N = Integer.parseInt(st.nextToken()); //악보 수
 		int Q = Integer.parseInt(st.nextToken()); //질문의 개수
 
-		int time[] = new int[Q];
+		List<Integer> list = new ArrayList<>();
 
-		for (int i = 0; i < Q; i++) {
-			time[i] = Integer.parseInt(br.readLine()); //시간 
+
+		//각 악보가 차지하는 시간(초)
+		for (int i = 1; i <= N; i++) { //악보는 1부터 시작
+			int time = Integer.parseInt(br.readLine()); 
+
+			//시간만큼 반복하여 곡 번호 저장
+			for (int j = 0; j < time; j++) {
+				list.add(i); 
+			}
 		}
 
+		//해당 초에 부르는 악보의 번호 구하기
+		for (int i = 0; i < Q; i++) {
+			int cnt = Integer.parseInt(br.readLine());
+			bw.write(list.get(cnt) + "\n");
+		}
 		bw.flush();
 		bw.close();
 	}
