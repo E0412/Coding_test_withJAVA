355b4d14e3bf308e1dc80a88bda6d259ac572898
gayoung choi <cy614@naver.com>
2023-05-31 21:33:05 +0900

    feat : 가장 많은 글자(문자열), 해결

--- Baekjoon/solution/B_1371.java
+++ Baekjoon/solution/B_1371.java
@@ -8,21 +8,32 @@ public class B_1371 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st  = new StringTokenizer(br.readLine());	
 
 		int[] arr = new int[26]; //알파벳 배열
-		String s = br.readLine();
+		String s; // = br.readLine()
 
-		while(s != null) {
-			for (int i = 0; i < arr.length; i++) {
+		while((s = br.readLine()) != null) {
+			for (int i = 0; i < s.length(); i++) {
 				if(s.charAt(i) >= 'a' && s.charAt(i) <= 'z') {
 					arr[s.charAt(i) - 'a']++;
 				}
 			}
 		}
+
 		int max = 0;
 		//가장 많이 나온 문자를 출력
+		for (int i = 0; i < 26; i++) {
+			if(max < arr[i]) {
+				max = arr[i];
+			}
+		}
 
+		for (int i = 0; i < 26; i++) {
+			if(max == arr[i]) {
+				char answer = (char) (i + 'a');
+				bw.write(answer);
+			}
+		}
 		bw.flush();
 		bw.close();
 	}
