74fa6b58acb9cd783ba37cd1ac7dad87bc9f0df8
gayoung choi <cy614@naver.com>
2023-06-20 23:02:45 +0900

    feat : 6단계 법칙(수정)

--- Baekjoon/solution/B_1389.java
+++ Baekjoon/solution/B_1389.java
@@ -10,6 +10,7 @@ import java.util.*;
  */
 public class B_1389 {
 
+	static final int tmp = -1;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,6 +21,18 @@ public class B_1389 {
 		int M = Integer.parseInt(st.nextToken());
 		int[][] arr = new int[N + 1][N + 1];
 
+
+		//초기값 설정
+		for (int i = 0; i <= N; i++) {
+			for (int j = 0; j <= N; j++) {
+				arr[i][j] = tmp;
+
+				if(i == j) {
+					arr[i][j] = 0; 
+				}
+			}
+		}
+
 		for (int i = 0; i < M; i++) {
 			st  = new StringTokenizer(br.readLine());
 
@@ -29,6 +42,11 @@ public class B_1389 {
 			//양방향 그래프 할당 
 			arr[x][y] = arr[y][x] = 1;  
 		}
+
+		int result = tmp;
+		int lo = -1;
+
+		//케빈 베이컨의 수가 가장 작은 인덱스 탐색
 
 		bw.flush();
 		bw.close();
