8aa07fcc23e37acd269afc371f0c3d6fbd238f2f
gayoung choi <cy614@naver.com>
2023-06-25 20:19:25 +0900

    feat : 귀걸이, 수정(2)

--- Baekjoon/solution/B_1380.java
+++ Baekjoon/solution/B_1380.java
@@ -1,7 +1,7 @@
 package solution;
 
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 //귀걸이 
 /*
@@ -12,17 +12,42 @@ public class B_1380 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		int cnt = Integer.parseInt(br.readLine()); //여학생의 수
-		String name = ""; //여학생들의 이름 
+		int s_cnt = 1; //시나리오 번호
 
-		for (int i = 0; i < cnt; i++) {
-			name = br.readLine();
-		}
+		while(true) {
+			String input = ""; //A 또는 B 입력
 
-		//여학생 번호와 'A'또는 'B' 입력 
-		StringTokenizer st  = new StringTokenizer(br.readLine());
-		for (int i = 0; i < 2*cnt - 1; i++) {
+			String g_cnt = br.readLine(); //압수당한 여학생 수 
+			int N = Integer.parseInt(g_cnt);
 
+			if(N == 0) {
+				break;
+			}
+
+			//이름 저장 배열
+			String[] name = new String[N];
+			for (int i = 0; i < N; i++) {
+				name[i] = br.readLine();
+			}
+
+			ArrayList<String> list = new ArrayList<>(); //번호를 입력받을 리스트
+
+			//여학생 번호와 'A'또는 'B' 입력 
+			for (int i = 0; i < 2*N - 1; i++) {
+				StringTokenizer st  = new StringTokenizer(br.readLine());
+				String num = st.nextToken();
+				
+				//번호가 겹치는 경우 삭제 
+				if(list.contains(num)) {
+					list.remove(num);
+				} else {
+					list.add(num);
+				}
+				input = br.readLine();
+			}
+
+			Integer result = Integer.parseInt(list.get(0)) - 1;
+			bw.write(s_cnt + " " + name[result]);
 		}
 
 		bw.flush();
