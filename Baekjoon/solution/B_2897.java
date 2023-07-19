ffcf7b54cfb51a8cdb5b0b093d2b5227315feba7
gayoung choi <cy614@naver.com>
2023-07-19 22:39:07 +0900

    feat : 몬스터 트럭(구현), 해결

--- Baekjoon/solution/B_2897.java
+++ Baekjoon/solution/B_2897.java
@@ -21,20 +21,51 @@ public class B_2897 {
 				arr[i][j] = in.charAt(j);
 			}
 		}
-		
+
+		int zero = 0, one = 0, two = 0, three = 0, four = 0;
+
 		for (int i = 0; i < R - 1; i++) {
 			for (int j = 0; j < C - 1; j++) {
 				int cnt = 0; //공간의 개수 
-				
+
 				//#인 빌딩은 부술 수 없다
 				if(arr[i][j] == '#' || arr[i + 1][j] == '#' || arr[i][j + 1] == '#' || arr[i + 1][j + 1] == '#') 
 					continue;
-				//X의 개수만큼 카운트한다
-				if(arr[i][j] == 'X' || arr[i + 1][j] == 'X' || arr[i][j + 1] == 'X' || arr[i + 1][j + 1] == 'X') 
-					cnt++;
+
+				//2행 2열 탐색
+				for (int m = i; m < i + 2; m++) {
+					for (int n = j; n < j + 2; n++) {
+						//X의 개수만큼 센다
+						if(arr[m][n] == 'X') cnt++;
+
+						if(m== i + 1 && n == j + 1) {
+							switch (cnt) {
+							case 0:
+								zero++;
+								break;
+
+							case 1:
+								one++;
+								break;
+
+							case 2:
+								two++;
+								break;
+
+							case 3:
+								three++;
+								break;
+
+							case 4:
+								four++;
+								break;
+							}
+						}
+					}
+				}
 			}
 		}
-		
+		bw.write(zero + "\n" + one + "\n" + two + "\n" + three + "\n" + four + "\n");
 		bw.flush();
 		bw.close();
 	}
