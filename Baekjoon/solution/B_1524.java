f7032bbef953e9979c78831ba9b47af040b2b808
gayoung choi <cy614@naver.com>
2023-08-04 23:43:05 +0900

    feat : 세준세비(구현), 해결

--- Baekjoon/solution/B_1524.java
+++ Baekjoon/solution/B_1524.java
@@ -15,23 +15,39 @@ public class B_1524 {
 
 		int T = Integer.parseInt(br.readLine());
 
-		st  = new StringTokenizer(br.readLine());	
 		for (int i = 0; i < T; i++) {
+			String tmp = br.readLine(); //빈칸 입력
+			st  = new StringTokenizer(br.readLine());	
 			int N = Integer.parseInt(st.nextToken());
 			int M = Integer.parseInt(st.nextToken());
 
-			int S = 0; //세준
-			int B = 0; //세비
+			//가장 강한 병사의 힘을 저장
+			int SJ = 0; //세준
+			int SB = 0; //세비
 
-			if(S > B) {
-				bw.write("S");
-			} else if(S < B) {
-				bw.write("B");
+			st  = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				int sej = Integer.parseInt(st.nextToken());
+
+				if(SJ < sej) SJ = sej; //강한 병사의 힘 저장
+			}
+
+			st  = new StringTokenizer(br.readLine());
+			for (int j = 0; j < M; j++) {
+				int seb = Integer.parseInt(st.nextToken());
+
+				if(SB < seb) SB = seb; //강한 병사의 힘 저장
+			}
+
+			//승자 출력
+			if(SJ > SB) {
+				bw.write("S" + "\n");
+			} else if(SJ < SB) {
+				bw.write("B" + "\n");
 			} else {
-				bw.write("C");
+				bw.write("S" + "\n"); //양쪽 모두 제일 강한 병사가 있는 경우 세준이 이긴다
 			}
 		}
-
 		bw.flush();
 		bw.close();
 	}
