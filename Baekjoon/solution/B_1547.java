0d355e2e9d99070a7cca6fc15e3ef59f83a50b9b
gayoung choi <cy614@naver.com>
2023-08-13 14:56:59 +0900

    feat : 공(구현), 해결

--- Baekjoon/solution/B_1547.java
+++ Baekjoon/solution/B_1547.java
@@ -4,21 +4,27 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 //공
+/*
+ * 공이 들어있는 컵의 번호를 출력한다.
+ */
 public class B_1547 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		int M = Integer.parseInt(br.readLine()); //컵의 위치를 바꾼 횟수
+		int cup = 1; //컵의 위치 
 
-		for (int i = 1; i <= 50; i++) {
+		for (int i = 0; i < M; i++) {
 			StringTokenizer st  = new StringTokenizer(br.readLine());	
 			int X = Integer.parseInt(st.nextToken()); //컵 
 			int Y = Integer.parseInt(st.nextToken());
-			
-			//컵의 위치를 바꾼 후 공이 있는 번호 출력
-		}
 
+			if (cup == X) //컵의 위치가 같은 경우 
+				cup = Y; //또 다른 컵의 번호가 공의 위치가 된다
+			else if (cup == Y) cup = X; 
+		}
+		bw.write(cup + "");
 		bw.flush();
 		bw.close();
 	}
