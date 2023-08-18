ed75872c5fced52bc191960e17fa0cee03c418bb
gayoung choi <cy614@naver.com>
2023-08-18 23:54:42 +0900

    feat : 꼬리를 무는 숫자 나열

---
+++ Baekjoon/solution/B_1598.java
@@ -0,0 +1,23 @@
+package solution;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+//꼬리를 무는 숫자 나열
+public class B_1598 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st  = new StringTokenizer(br.readLine());	
+	
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		int x1, x2, y1, y2; //좌표
+		
+		//동서방향거리와 남북방향거리의 합을 구한다 
+		
+		bw.flush();
+		bw.close();
+	}
+}
