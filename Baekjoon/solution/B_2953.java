a08c3a3f3283663a8b77a16b105d2fa38818591c
gayoung choi <cy614@naver.com>
2023-08-22 21:04:11 +0900

    feat : 나는 요리사다

---
+++ Baekjoon/solution/B_2953.java
@@ -0,0 +1,35 @@
+package solution;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+//나는 요리사다
+/*
+ * 각 참가자가 얻은 평가 점수가 주어졌을 때, 
+ * 우승자와 그의 점수를 구하는 프로그램을 작성한다
+ */
+public class B_2953 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;	
+
+		int[][] arr = new int[5][4];
+
+		int total = 0; //평가점수 총합
+		int max = 0; //최대 점수
+		int num = 0; //참가자 번호
+
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 4; j++) {
+				st  = new StringTokenizer(br.readLine());	
+				arr[i][j] = Integer.parseInt(st.nextToken());
+				total += arr[i][j]; //점수의 합 저장
+			}
+		}
+
+
+		bw.flush();
+		bw.close();
+	}
+}
