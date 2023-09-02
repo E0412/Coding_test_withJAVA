d3b8b608005c42292939111cd40741ea31bee638
gayoung choi <cy614@naver.com>
2023-09-02 23:11:28 +0900

    feat : 백설 공주와 일곱 난쟁이(브루트포스), 해결

--- Baekjoon/solution/B_3040.java
+++ Baekjoon/solution/B_3040.java
@@ -7,16 +7,30 @@ public class B_3040 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
+
 		int[] arr = new int[9]; 
 		int total = 0; //난쟁이 숫자의 합
-		
+
 		for (int i = 0; i < 9; i++) {
 			arr[i] = Integer.parseInt(br.readLine());
 			total += arr[i];
 		}
-		
-		
+		for (int i = 0; i < 8; i++) {
+			for (int j = i + i; j < 9; j++) {
+				int fake = arr[i] + arr[j]; //가짜 난쟁이 숫자의 합
+				
+				//난쟁이 - 가짜 난쟁이의 값이 100이 되는 경우
+				if(total - fake == 100) {
+					arr[i] = 0; arr[j] = 0; //숫자를 찾으면 수를 0으로 바꿔준다
+					
+					for (int k = 0; k < 9; k++) {
+						if(k != i && k != j) {
+							bw.write(arr[k] + "\n");
+						}
+					}
+				}
+			}
+		}
 		bw.flush();
 		bw.close();
 	}
