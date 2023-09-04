87331d150c21187c51fb35a62b73ec69887a2141
gayoung choi <cy614@naver.com>
2023-09-04 23:18:07 +0900

    feat : 스캐너(문자열), 해결

--- Baekjoon/solution/B_3035.java
+++ Baekjoon/solution/B_3035.java
@@ -9,22 +9,31 @@ public class B_3035 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st  = new StringTokenizer(br.readLine());
-		
-		char arr[][] = new char[51][51];
-		
+
 		int R = Integer.parseInt(st.nextToken()); //1과 50 사이의 정수
 		int C = Integer.parseInt(st.nextToken());
 		int ZR = Integer.parseInt(st.nextToken()); //1과 5 사이의 정수 
 		int ZC = Integer.parseInt(st.nextToken());
-				
+
+		String[] arr = new String[R];
+
 		//신문기사 입력  
 		for (int i = 0; i < arr.length; i++) {
-			for (int j = 0; j < arr.length; j++) {
-				arr[i][j] = (char) br.read();
+			arr[i] = br.readLine();
+		}
+
+		//R * ZR, C * ZC 만큼 출력한다
+		for (int i = 0; i < R; i++) {
+			for (int j = 0; j < ZR; j++) {
+				for (int k = 0; k < C; k++) {
+					for (int l = 0; l < ZC; l++) {
+						bw.write(arr[i].charAt(k));
+					}
+				}
+				bw.write("\n");
 			}
 		}
-		
 		bw.flush();
 		bw.close();
-		}
+	}
 }
