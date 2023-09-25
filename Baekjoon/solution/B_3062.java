c9d9418b106d961af48dd299147bdc4249a3823a
gayoung choi <cy614@naver.com>
2023-09-25 20:13:23 +0900

    feat : 수 뒤집기(구현), 해결

--- Baekjoon/solution/B_3062.java
+++ Baekjoon/solution/B_3062.java
@@ -18,8 +18,24 @@ public class B_3062 {
 			for (int j = in.length() - 1; j >= 0; j--) {
 				reverse += in.charAt(j);
 			}
-		}
 
+			int sum = Integer.parseInt(in) + Integer.parseInt(reverse);
+			String con = Integer.toString(sum); //변환
+			boolean sym = true;
+
+			for (int j = 0; j < con.length()/2; j++) {
+				char first = con.charAt(j);
+				char last = con.charAt(con.length() - j - 1);
+
+				//대칭 검사
+				if(first != last) {
+					sym = false;
+					break;
+				}
+			}
+			if(sym) bw.write("YES" + "\n");
+			else bw.write("NO" + "\n");
+		}
 		bw.flush();
 		bw.close();
 	}
