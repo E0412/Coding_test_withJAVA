dff48c680c414f1ac89cff0a07dcd289280a80fb
gayoung choi <cy614@naver.com>
2023-09-27 22:19:47 +0900

    feat : 짐 챙기는 숌(그리디), 해결

--- Baekjoon/solution/B_1817.java
+++ Baekjoon/solution/B_1817.java
@@ -12,13 +12,28 @@ public class B_1817 {
 
 		int N = Integer.parseInt(st.nextToken()); //책의 개수
 		int M = Integer.parseInt(st.nextToken()); //최대 무게
-		
+
 		//책의 개수가 0인경우
 		if(N == 0) {
 			bw.write(0 + "");
+			bw.flush();
 			return;
 		}
-		
+
+		int cnt = 1;
+		int max = M;
+		st  = new StringTokenizer(br.readLine());	
+		while(N-- > 0) { //책의 개수가 0이상인 경우
+			int tmp = Integer.parseInt(st.nextToken());
+
+			if(M - tmp >= 0) {
+				M -= tmp;
+			} else {
+				cnt++;
+				M = max - tmp;
+			}
+		}
+		bw.write(cnt + "");
 		bw.flush();
 		bw.close();
 	}
