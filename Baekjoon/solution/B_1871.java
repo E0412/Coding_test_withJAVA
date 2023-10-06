c85a0a1c4ad30dda77dc5fd6a6648b314f21bec6
gayoung choi <cy614@naver.com>
2023-10-06 23:16:41 +0900

    feat : 좋은 자동차 번호판(구현), 해결

--- Baekjoon/solution/B_1871.java
+++ Baekjoon/solution/B_1871.java
@@ -13,20 +13,26 @@ public class B_1871 {
 
 		for (int i = 0; i < N; i++) {
 			String in = br.readLine();
+			
 			StringTokenizer st  = new StringTokenizer(in, "-");	
-
 			String a = st.nextToken(); //첫번째 번호판
 			String b = st.nextToken(); //두번째 번호판
+			
 			int a_num = 0;
 			for (int j = 0; j < a.length(); j++) {
-				int sum = a.charAt(j) - 97;
+				int sum = a.charAt(j) - 'A';
 				sum *= Math.pow(26, 2 - j);
 				a_num += sum;
 			}
-			int b_num = 0;
+			int b_num = Integer.parseInt(b);
+			
 			//좋은 번호판이면 "nice"를, 아니면 "not nice"를 출력
+			if(Math.abs(a_num - b_num) <= 100) {
+				bw.write("nice\n");
+			} else {
+				bw.write("not nice\n");
+			}
 		}
-
 		bw.flush();
 		bw.close();
 	}
