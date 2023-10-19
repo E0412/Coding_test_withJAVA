ef8ca9462d96e82b4b7ceae9fee1a683b9b78fa4
gayoung choi <cy614@naver.com>
2023-10-19 17:44:03 +0900

    feat : 상근이의 체스판(구현), 해결

--- Baekjoon/solution/B_3076.java
+++ Baekjoon/solution/B_3076.java
@@ -12,21 +12,37 @@ public class B_3076 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st  = new StringTokenizer(br.readLine());
-		
+
 		int R = Integer.parseInt(st.nextToken());
 		int C = Integer.parseInt(st.nextToken());
+
+		st  = new StringTokenizer(br.readLine());
 		int A = Integer.parseInt(st.nextToken());
 		int B = Integer.parseInt(st.nextToken());
-		
+
 		char[][] arr = new char[R * A][C * B];
-		
-		for (int i = 0; i < R * A; i++) {
-			for (int j = 0; j < C * B; j++) {
+
+		for (int i = 0; i < R * A; i+= A) {
+			for (int j = 0; j < C * B; j+= B) {
 				char ch = '.';
 				//검은 체스판
+				if((i / A % 2 == 0 && j / B % 2 == 0) || (i / A % 2 != 0 && j / B % 2 != 0)) {
+					ch = 'X';
+				}
+
+				for (int n = i; n < i + A; n++) {
+					for (int m = j; m < j + B; m++) {
+						arr[n][m] = ch; 
+					}
+				}
 			}
 		}
-		
+
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < R * A; i++) {
+			sb.append(arr[i]).append("\n");
+		}
+		bw.write(sb.toString());
 		bw.flush();
 		bw.close();
 	}
