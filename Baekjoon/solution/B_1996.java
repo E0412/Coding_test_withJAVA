8540684bace15d8fe6b6f693ee882d088bafeb2f
gayoung choi <cy614@naver.com>
2023-10-22 20:32:13 +0900

    feat : 지뢰 찾기(2)

--- Baekjoon/solution/B_1996.java
+++ Baekjoon/solution/B_1996.java
@@ -13,25 +13,45 @@ public class B_1996 {
 	static int dy[] = {-1, -1, -1, 0, 0, 1, 1, 1};
 	static char tmp[][]; //지뢰 상태
 	static char map[][]; //결과
-	
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		int N = Integer.parseInt(br.readLine());
-		
+
 		tmp= new char[N][N];
 		map = new char[N][N];
-		
+
 		for (int i = 0; i < N; i++) {
 			String s = br.readLine();
 			for (int j = 0; j < N; j++) {
 				tmp[i][j] = s.charAt(j); 
 			}
 		}
-		
+
 		//지뢰 탐색
-		
+		for (int x = 0; x < N; x++) {
+			for (int y = 0; y < N; y++) {
+				//지뢰가 있는 경우
+				if(tmp[x][y] != '.') {
+					map[x][y] = '*'; 
+				} else {
+					int cnt = 0; //주변 지뢰의 수
+
+					for (int k = 0; k < 8; k++) {
+						int nx = x + dx[k];
+						int ny = y + dy[k];
+
+						//숫자인 경우
+						if(nx >= 0 && nx < N && ny >= 0 && ny < N && tmp[nx][ny] != '.') {
+							cnt += (tmp[nx][ny] - '0'); //지뢰 추가
+						}
+					}
+				}
+			}
+		}
+
 		bw.flush();
 		bw.close();
 	}
