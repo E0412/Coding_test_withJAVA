53f65083a0c0229120d8c0180a8ef11163843ab1
gayoung choi <cy614@naver.com>
2023-10-26 18:36:53 +0900

    feat : 양(2)

--- Baekjoon/solution/B_3184.java
+++ Baekjoon/solution/B_3184.java
@@ -13,28 +13,48 @@ public class B_3184 {
 	static int O, V, R, C;
 	static int dx[] = {-1, 0, 1, 0};
 	static int dy[] = {0, 1, 0, -1};
-	
+
 	static char[][] graph;
 	static boolean[][] visited;
+
+	static void DFS(int x, int y) {
+		
+	}
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st  = new StringTokenizer(br.readLine());
-		
+
 		R = Integer.parseInt(st.nextToken()); //행의 수
 		C = Integer.parseInt(st.nextToken()); //열의 수
-		
+
 		graph = new char[R][C];
 		visited = new boolean[R][C];
-		
+
 		for (int i = 0; i < R; i++) {
 			String s = br.readLine();
 			for (int j = 0; j < C; j++) {
 				graph[i][j] = s.charAt(j); 
 			}
 		}
-		
+
+		int sheep = 0;
+		int wolf = 0;
+
+		for (int i = 0; i < R; i++) {
+			for (int j = 0; j < C; j++) {
+				if(graph[i][j]== '#') continue; //울타리인 경우
+				//울타리가 아닌 경우
+				O = V = 0; //O는 양, V는 늑대
+				DFS(i, j);
+				//양이 더 많은 경우
+				if(O > V) sheep += O;
+				//늑대가 더 많은 경우 
+				else wolf += V;
+			}
+		}
+
 		bw.flush();
 		bw.close();
 	}
