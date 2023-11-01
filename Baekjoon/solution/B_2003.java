36da5767a0733f6ecb4a553bed68b6b39319e807
gayoung choi <cy614@naver.com>
2023-11-01 21:36:15 +0900

    feat : 수들의 합 2(투 포인터), 해결

--- Baekjoon/solution/B_2003.java
+++ Baekjoon/solution/B_2003.java
@@ -20,7 +20,26 @@ public class B_2003 {
 			arr[i] = Integer.parseInt(st.nextToken()); 
 		}
 		//경우의 수 구하기
+		int start = 0, end = 0, result = 0, sum = 0;
 
+		while(true) {
+			//원소의 합이 M이 되는 경우 
+			if (sum == M) result++; //경우의 수를 더해준다
+
+			//원소의 합이 M보다 큰 경우 원소들을 빼준다
+			if(sum >= M) {
+				sum -= arr[start++];
+			} 
+			//배열의 끝에 도착한 경우 종료
+			else if(end == N) {
+				break;
+			} 
+			//그 외에는 합한다 
+			else{
+				sum += arr[end++];
+			}
+		}
+		bw.write(result + "");
 		bw.flush();
 		bw.close();
 	}
