ce2c5f6124cbf8f7fe55a26ecb0131f6a504141b
gayoung choi <cy614@naver.com>
2023-11-06 22:45:06 +0900

    feat : 반올림(수학), 해결

--- Baekjoon/solution/B_2033.java
+++ Baekjoon/solution/B_2033.java
@@ -8,21 +8,20 @@ public class B_2033 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		int N = Integer.parseInt(br.readLine());
+		double N = Double.parseDouble(br.readLine());
+		int p = 1; //자릿수
 
-		int p = 1;
-		int ans = 0;
-
-		//정수 N이 10보다 큰 경우, 일의 자리에서 반올림
-		while(N > 0) {
-			int com = 10;
-
-			if(N > com) {
-				int tmp = (int) Math.pow(10, p);
-				com *= 10; //자릿수 증가 
-			}
+		//정수 N이 10보다 큰 경우, 일의 자리에서 반올림 -> 자릿수를 증가시키며 반복
+		for (int i = 10; i < 10000; i *= 10) {
+			if(N > i) {
+				int pow = (int) Math.pow(10, p); //10의 p(자릿수)제곱
+				N /= pow;
+				N = Math.round(N); //반올림
+				N *= pow;
+			} else break;
+			p++; //자릿수 증가
 		}
-
+		bw.write((int)N + "");
 		bw.flush();
 		bw.close();
 	}
