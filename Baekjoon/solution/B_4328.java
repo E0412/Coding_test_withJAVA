e8be96e8685456798a438fb219d89037451d9f4f
gayoung choi <cy614@naver.com>
2023-11-25 20:10:13 +0900

    feat : 기초 나머지 계산(큰 수 연산), 해결

--- Baekjoon/solution/B_4328.java
+++ Baekjoon/solution/B_4328.java
@@ -1,35 +1,28 @@
 package solution;
 
 import java.io.*;
-import java.util.StringTokenizer;
+import java.math.BigInteger;
 
 //기초 나머지 계산
 public class B_4328 {		
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
 
-		int b = 0; int p = 0; int m = 0;
+		while(true) {
+			String in = br.readLine();
+			if("0".equals(in)) {
+				break;
+			}
+			String[] arr = in.split(" ");
+			int b = Integer.parseInt(arr[0]);
 
-		//반복문으로 입력받으려면 배열에 담는걸 고려
-		StringTokenizer st  = new StringTokenizer(br.readLine());
-		b = Integer.parseInt(st.nextToken());
-		p = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-
-		int mod = p % m;
-
-		if(b == 2) {
-			String result = Integer.toBinaryString(mod);
-			bw.write(result);
-		} 
-		if (b == 10) {
-			String result = Integer.toHexString(mod);
-			bw.write(result);
+			sb.append(new BigInteger(arr[1], b)
+					.mod(new BigInteger(arr[2], b))
+					.toString(b)).append("\n");
 		}
-		if(b == 0) { //종료조건
-			return;
-		}
+		bw.write(sb.toString());
 		bw.flush();
 		bw.close();
 	}
