3f3076534f9899e4140df4950ed8d96a36cb5001
gayoung choi <cy614@naver.com>
2023-11-29 21:00:35 +0900

    feat : 생태학(TreeMap 활용), 해결

--- Baekjoon/solution/B_4358.java
+++ Baekjoon/solution/B_4358.java
@@ -1,6 +1,7 @@
 package solution;
 
 import java.io.*;
+import java.util.Map;
 import java.util.TreeMap;
 
 //생태학
@@ -11,18 +12,21 @@ public class B_4358 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
+
 		TreeMap<String, Integer> map = new TreeMap<>(); //자동으로 정렬되는 TreeMap 이용
 		String tree = ""; 
 		double percent = 0; //종이 차지하는 비율
-		
+
 		while((tree = br.readLine()) != null) {
 			//종의 이름, 개수 입력
 			map.put(tree, map.getOrDefault(tree, 0) + 1); //해당하는 값에 + 1을 한다  
 			percent++; 
 		}
-		
-		bw.flush();
+
+		for(Map.Entry<String, Integer> o : map.entrySet()) {
+			bw.write(o.getKey() + " " + String.format("%.4f", o.getValue() / percent * 100) + "\n");
+			bw.flush();
+		}
 		bw.close();
 	}
 }
