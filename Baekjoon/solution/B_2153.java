319218b58c93a3816a14b85e1b1285495c21e6b8
gayoung choi <cy614@naver.com>
2023-12-04 22:03:15 +0900

    feat : 소수 단어(문자열), 해결

--- Baekjoon/solution/B_2153.java
+++ Baekjoon/solution/B_2153.java
@@ -17,9 +17,22 @@ public class B_2153 {
 
 			if(ch >= 'a' && ch <= 'z') {
 				sum += (int) ch - 96;
+			} else if(ch >= 'A' && ch <= 'Z') {
+				sum += (int) ch - 38;
 			}
 		}
-
+		//소수인지 확인
+		for (int i = 2; i < sum; i++) {
+			if(sum % i == 0) {
+				check = true;
+			}
+		}
+		//출력
+		if(check) {
+			bw.write("It is not a prime word.");
+		} else {
+			bw.write("It is a prime word.");
+		}
 		bw.flush();
 		bw.close();
 	}
