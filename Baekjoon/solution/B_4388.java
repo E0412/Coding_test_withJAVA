33b3247fc41a375581b11edca05b455c7500c859
gayoung choi <cy614@naver.com>
2023-12-27 12:41:54 +0900

    feat : 받아올림(1)

---
+++ Baekjoon/solution/B_4388.java
@@ -0,0 +1,25 @@
+package solution;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+//받아올림
+public class B_4388 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		while(true) {
+			StringTokenizer st  = new StringTokenizer(br.readLine());
+			int n = Integer.parseInt(st.nextToken());
+			int m = Integer.parseInt(st.nextToken());
+
+			if(n + m == 0) break;
+			int cnt = 0; //받아올림 개수
+			int tmp = 0;
+		}
+
+		bw.flush();
+		bw.close();
+	}
+}
