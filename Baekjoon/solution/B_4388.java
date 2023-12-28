aefaec7435893baa13a88789c21e5ad694b12700
gayoung choi <cy614@naver.com>
2023-12-28 12:21:58 +0900

    feat : 받아올림(수학), 해결

--- Baekjoon/solution/B_4388.java
+++ Baekjoon/solution/B_4388.java
@@ -17,8 +17,19 @@ public class B_4388 {
 			if(n + m == 0) break;
 			int cnt = 0; //받아올림 개수
 			int tmp = 0;
-		}
 
+			while(n != 0 || m != 0) {
+				int ncnt = n % 10;
+				int mcnt = m % 10;
+				n /= 10;
+				m /= 10;
+				if(ncnt + mcnt + tmp >= 10) {
+					cnt++;
+					tmp = 1;
+				} else tmp = 0;
+			}
+			bw.write(cnt + "\n");
+		}
 		bw.flush();
 		bw.close();
 	}
