e174a972f372ba9c6409ee2b9b64a25c0089c6cd
gayoung choi <cy614@naver.com>
2024-01-01 20:39:37 +0900

    feat : 트ㅏㅊ;(1)

---
+++ Baekjoon/solution/B_4378.java
@@ -0,0 +1,26 @@
+package solution;
+
+import java.io.*;
+
+//트ㅏㅊ;
+/*
+* 입력된 글자의 한 칸 앞 글자를 출력하면 된다
+*/
+public class B_4378 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+
+		String keyboard = "`1234567890-=QWERTYUIOP[]\\ASDFGHJKL;'ZXCVBNM,./";
+		String in = "";
+		
+		//반복문 돌면서 이전 문자로 저장
+		while((in = br.readLine()) != null) {
+			
+		}
+		
+		bw.flush();
+		bw.close();
+	}
+}
