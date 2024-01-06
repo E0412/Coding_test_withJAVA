4feb317bcdd6d5f5dc34f1aa4ffed2aa6a9de5c3
gayoung choi <cy614@naver.com>
2024-01-06 20:14:50 +0900

    feat : 포도주 시식(dp), 해결

--- Baekjoon/solution/B_2156.java
+++ Baekjoon/solution/B_2156.java
@@ -17,8 +17,14 @@ public class B_2156 {
 
 		int[] amt = new int[N + 1];
 		amt[1] = arr[1];
-
-
+		
+		if(N > 1) {
+			amt[2] = arr[1] + arr[2];
+		}
+		for (int i = 3; i <= N; i++) {
+			amt[i] = Math.max(amt[i - 1], Math.max(amt[i - 2] + arr[i], amt[i - 3] + arr[i - 1] + arr[i])); 
+		}
+		bw.write(amt[N] + "");
 		bw.flush();
 		bw.close();
 	}
