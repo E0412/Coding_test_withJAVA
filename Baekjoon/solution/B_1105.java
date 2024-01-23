4b9aa02bbb430671e205430010e1593b9cdb5376
gayoung choi <cy614@naver.com>
2024-01-23 22:58:06 +0900

    feat : 팔(그리디), 해결

--- Baekjoon/solution/B_1105.java
+++ Baekjoon/solution/B_1105.java
@@ -9,20 +9,30 @@ public class B_1105 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st  = new StringTokenizer(br.readLine());
-		
+
 		String L = st.nextToken();
 		String R = st.nextToken();
-		
+
 		int cnt = 0;
-		
+		int tmp;
+
 		//자리수가 다른 경우
 		if(L.length() != R.length()) {
 			bw.write(cnt + "");
 		}
-		
 		//자리수가 같은 경우 
-		
-		
+		else {
+			tmp = 0;
+			//수를 비교한다
+			while(tmp < L.length() && L.charAt(tmp) == R.charAt(tmp)) {
+				//8이 있는 경우 개수에 더한다
+				if(L.charAt(tmp) == '8') {
+					cnt++;
+				}
+				tmp++;
+			}
+			bw.write(cnt + "");
+		}
 		bw.flush();
 		bw.close();
 	}
