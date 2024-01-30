4898347d00a413820ea87b5deca6d48268508f55
gayoung choi <cy614@naver.com>
2024-01-30 22:11:39 +0900

    feat : 가위 바위 보?(1)

---
+++ Baekjoon/solution/B_4493.java
@@ -0,0 +1,39 @@
+package solution;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+//가위 바위 보?
+public class B_4493 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int N = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < N; i++) {
+			int M = Integer.parseInt(br.readLine());
+			int cnt = 0;
+
+			for (int j = 0; j < M; j++) {
+				StringTokenizer st  = new StringTokenizer(br.readLine());
+				String p1 = st.nextToken();
+				String p2 = st.nextToken();
+
+				//p1이 이기는 경우 cnt++
+				if(p1.equals("P") && p2.equals("R") || p1.equals("R") && p2.equals("S")
+						|| p1.equals("S") && p2.equals("P")) {
+					cnt++;
+				} 
+				//p2가 이기는 경우 cnt--
+				else if(p1.equals("R") && p2.equals("P") || p1.equals("S") && p2.equals("R")
+						|| p1.equals("P") && p2.equals("S")) {
+					cnt--;
+				}
+			}
+			//cnt가 양수이면 p1이 승자, 음수이면 p2가 승자 
+		}
+		bw.flush();
+		bw.close();
+	}
+}
