5947218904cb7129ad80cdc5f1cdcde44a0c8d9d
gayoung choi <cy614@naver.com>
2024-02-03 01:05:42 +0900

    feat : RGB 거리(DP), 해결

--- Baekjoon/solution/B_1149.java
+++ Baekjoon/solution/B_1149.java
@@ -1,6 +1,7 @@
 package solution;
 
 import java.io.*;
+import java.util.StringTokenizer;
 
 //RGB 거리
 /* 1번 집의 색은 2번 집의 색과 같지 않아야 한다.
@@ -15,13 +16,28 @@ public class B_1149 {
 		int N = Integer.parseInt(br.readLine());
 		int[][] arr = new int[N][3];
 
-		for (int i = 2; i <= N; i++) {
-			String in = br.readLine();
-			arr[i][0] = (Integer.parseInt(in.split(" ")[0]));
-			arr[i][1] = (Integer.parseInt(in.split(" ")[1]));
-			arr[i][2] = (Integer.parseInt(in.split(" ")[2]));
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			if(st.hasMoreTokens()) {
+				arr[i][0] = Integer.parseInt(st.nextToken());
+				arr[i][1] = Integer.parseInt(st.nextToken());
+				arr[i][2] = Integer.parseInt(st.nextToken());
+			}
 		}
 
+		for (int i = 1; i < N; i++) {
+			//i번째 집이 r, g, b일 때 
+			int r = arr[i][0];
+			int g = arr[i][1];
+			int b = arr[i][2];
+
+			//최솟값 저장
+			arr[i][0] = r + Math.min(arr[i-1][1], arr[i-1][2]);
+			arr[i][1] = g + Math.min(arr[i-1][0], arr[i-1][2]);
+			arr[i][2] = b + Math.min(arr[i-1][1], arr[i-1][0]);
+		}
+		int cost = Math.min(Math.min(arr[N - 1][0], arr[N-1][1]), arr[N-1][2]);
+		bw.write(cost + "");
 		bw.flush();	
 		bw.close();
 	}
