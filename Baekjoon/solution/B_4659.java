ca4e8edd62662c7d927df65affee98462d7321ba
gayoung choi <cy614@naver.com>
2024-02-15 20:16:47 +0900

    feat : 비밀번호 발음하기(구현), 해결

--- Baekjoon/solution/B_4659.java
+++ Baekjoon/solution/B_4659.java
@@ -8,35 +8,51 @@ public class B_4659 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		String pw = "";
-		boolean check; //글자가 모음인 경우 true
-		int cnt = 0; //자음 연속 글자 확인, 3 이상일시 check = false
-		int con = 0; //모음 연속 글자 확인
+		String in = "";
 
+		while(!(in = br.readLine()).equals("end")) {
+			boolean g_chk = false; //모음이 3개 이상인 경우 false
+			boolean c_chk = true; //자음이 3개 이상인 경우 false
+			boolean eo_chk = true; //그 외 조건
+			int g_cnt = 0; //모음 연속 글자 확인, 3 이상일시 g_chk = false
+			int c_cnt = 0; //자음 연속 글자 확인
 
-		while(true) {
-			String in = br.readLine();
+			for (int i = 0; i < in.length(); i++) {
+				char now = in.charAt(i);
 
-			if(in.equals("end")) break;
-			check = false;
-
-			for (int i = 1; i <= 20; i++) {
 				//높은 품질의 비밀번호 조건 1
-				if(in.charAt(i) == 'a' || in.charAt(i) == 'e' || in.charAt(i) == 'i'
-						|| in.charAt(i) == 'o' || in.charAt(i) == 'u') {
-					check = true;
-					cnt++;
-					if(cnt >= 3) check = false; break;
+				if(now == 'a' || now == 'e' || now == 'i' || now == 'o' || now == 'u') {
+					g_chk = true;
+					g_cnt++;
+					c_cnt = 0;
+					if(g_cnt >= 3) {
+						c_chk = false; break;
+					}
 				} 
 				//자음이 3개 연속으로 오는 경우
 				else {
-					con++;
-					if(cnt >= 3) check = false; break;
+					c_cnt++;
+					g_cnt = 0;
+					if(c_cnt >= 3) {
+						c_chk = false; break;
+					}
 				}
 				//같은 글자가 연속적으로 두번 오면 안되나, ee 와 oo는 허용
+				//-> 이 둘을 제외한 글자들이 연속된 경우 eo_chk = false
+				if(i >= 1) {
+					if(in.charAt(i) != 'e' && in.charAt(i) != 'o') {
+						if(in.charAt(i) == in.charAt(i - 1)) 
+							eo_chk = false; 
+					}
+				}
+			}
+			if(c_chk && g_chk && eo_chk) {
+				bw.write("<" + in + "> is acceptable.\n");
+			}
+			else {
+				bw.write("<" + in + "> is not acceptable.\n");
 			}
 		}
-
 		bw.flush();
 		bw.close();
 	}
