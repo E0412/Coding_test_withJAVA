aec55a0e68b9279febb16a6f4aa14a987336357d
gayoung choi <cy614@naver.com>
2024-02-18 20:32:49 +0900

    feat : 농구 경기(구현), 해결

--- Baekjoon/solution/B_1159.java
+++ Baekjoon/solution/B_1159.java
@@ -7,18 +7,26 @@ public class B_1159 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
+
 		int N = Integer.parseInt(br.readLine());
 		int[] arr = new int[26];
-		
+
 		for (int i = 0; i < N; i++) {
 			String in = br.readLine(); 
 			arr[in.charAt(0) - 'a']++; 
 		}
-		
+
 		//성의 첫 글자가 같은 선수 5명을 선발
-		
-		
+		boolean draft = false; //선발이 된 경우 true
+		for (int i = 0; i < 26; i++) {
+			if(arr[i] >= 5) {
+				bw.write((char)(i + 'a'));
+				draft = true;
+			} 
+		}
+		if(!draft) {
+			bw.write("PREDAJA");
+		}
 		bw.flush();
 		bw.close();
 	}
