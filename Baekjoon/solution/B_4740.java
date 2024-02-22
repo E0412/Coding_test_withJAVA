cbada444962fbccdbd3aba44b8f5155ec91aa23b
gayoung choi <cy614@naver.com>
2024-02-22 23:28:45 +0900

    feat : 거울, 오! 거울(1)

---
+++ Baekjoon/solution/B_4740.java
@@ -0,0 +1,21 @@
+package solution;
+
+import java.io.*;
+
+//거울, 오! 거울
+public class B_4740 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		while(true) {
+			String in = br.readLine();
+			//*** 입력시 프로그램 종료
+			if(in.equals("***")) break;
+			//글자를 역순으로 바꾸어 출력 
+		}
+		
+		bw.flush();
+		bw.close();
+	}
+}
