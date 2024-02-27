b9ae263c71f7a6493bf48f1de81bf5d248c7c44c
gayoung choi <cy614@naver.com>
2024-02-27 20:37:59 +0900

    feat : 도비의 난독증 테스트(문자열), 해결

--- Baekjoon/solution/B_2204.java
+++ Baekjoon/solution/B_2204.java
@@ -1,6 +1,8 @@
 package solution;
 
 import java.io.*;
+import java.util.Arrays;
+import java.util.HashMap;
 
 //도비의 난독증 테스트
 public class B_2204 {
@@ -14,13 +16,20 @@ public class B_2204 {
 			if(N == 0) break;
 
 			String[] arr = new String[N];
+			HashMap<String, String> map = new HashMap<String, String>();
 
 			for (int i = 0; i < N; i++) {
 				arr[i] = br.readLine();
 			}
 			//사전상 가장 앞서는 단어 찾기
+			for (int i = 0; i < N; i++) {
+				String s = arr[i].toLowerCase(); //비교를 위해 소문자로 단어 저장
+				map.put(s, arr[i]); //소문자 문자열과 기존 문자열 저장
+				arr[i] = arr[i].toLowerCase();
+			}
+			Arrays.sort(arr);
+			bw.write(map.get(arr[0]) + "\n"); //사전상 가장 앞서는 단어 출력 
 		}
-
 		bw.flush();
 		bw.close();
 	}
