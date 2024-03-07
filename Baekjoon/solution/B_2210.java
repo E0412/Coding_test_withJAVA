542d35aede4ef25e0504b173506751696500961d
gayoung choi <cy614@naver.com>
2024-03-07 23:44:59 +0900

    feat : 숫자판 점프(2)

--- Baekjoon/solution/B_2210.java
+++ Baekjoon/solution/B_2210.java
@@ -2,6 +2,7 @@ package solution;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.StringTokenizer;
 
 //숫자판 점프 
@@ -14,7 +15,17 @@ public class B_2210 {
 	static ArrayList<String> list;
 
 	static void DFS(int x, int y, int d) {
-
+		if(d > 6) {
+			String s = "";
+			for (int i = 0; i < arr.length; i++) {
+				s += Integer.valueOf(arr[i]);
+			}
+			//중복되지 않는 경우 리스트에 추가
+			if(list.indexOf(s) < 0) list.add(s);
+			return;
+		}
+		//깊이(d)가 6이 될 때까지 배열 할당
+		
 	}
 
 	public static void main(String[] args) throws IOException {
