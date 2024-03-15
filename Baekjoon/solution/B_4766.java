a02e64fe414461e6c74e0723133bb4a9e0c5a27e
gayoung choi <cy614@naver.com>
2024-03-15 21:00:24 +0900

    feat : 일반 화학 실험(수학), 해결

--- Baekjoon/solution/B_4766.java
+++ Baekjoon/solution/B_4766.java
@@ -9,14 +9,16 @@ public class B_4766 {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		double N = Double.parseDouble(br.readLine());
-		
+
 		while(true) {
 			double M = Double.parseDouble(br.readLine());
-			
+
 			if(M == 999) break; //종료조건
-			
+
 			//각 온도와 이전 온도와의 차이 출력
-			
+			String answer = String.format("%.2f", (M - N)); //소수점 둘째자리까지 출력
+			bw.write(answer + "\n");
+			N = M; //현재값을 저장 -> 다시 연산
 		}
 		bw.flush();
 		bw.close();
