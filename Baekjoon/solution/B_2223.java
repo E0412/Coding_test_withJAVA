02b75bf6c6d505703b05d7f968f3088dae50b547
gayoung choi <cy614@naver.com>
2024-03-25 21:11:15 +0900

    feat : 금화(구현)

--- Baekjoon/solution/B_2223.java
+++ Baekjoon/solution/B_2223.java
@@ -15,13 +15,25 @@ public class B_2223 {
 		int m = Integer.parseInt(st.nextToken()); 
 
 		int result = 0;
+		int min = Integer.MAX_VALUE; 
 
 		for (int i = 0; i < m; i++) {
 			st  = new StringTokenizer(br.readLine());
 			int d = Integer.parseInt(st.nextToken()); //몬스터 거리
 			int s = Integer.parseInt(st.nextToken()); //움직이는 거리
-			//담을 수 있는 금화의 최대 개수 구하기
+			min = Math.min(min, (d - 1) / s);
 		}
+		//담을 수 있는 금화의 최대 개수 구하기
+		if(min == 0) {
+			result = 0;
+		} else if(t < min) {
+			result = t * x;
+		} else {
+			result = min * x;
+			t= t - min;
+			result += (t / 2) * x;
+		}
+		bw.write(result + "");
 		bw.flush();
 		bw.close();
 	}
