a8d9416602d541ad337b2aa4e78e5117b9fb2553
gayoung choi <cy614@naver.com>
2024-03-27 23:58:19 +0900

    feat : 화학식량(1)

---
+++ Baekjoon/solution/B_2257.java
@@ -0,0 +1,23 @@
+package solution;
+
+import java.io.*;
+import java.util.Stack;
+
+//화학식량
+public class B_2257 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		String in = br.readLine();
+		Stack<Integer> stack = new Stack<>();
+
+		for (int i = 0; i < in.length(); i++) {
+			char ch = in.charAt(i);
+			//화학식량 계산 
+		}
+
+		bw.flush();
+		bw.close();
+	}
+}
