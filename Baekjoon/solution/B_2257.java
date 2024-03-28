c18862faf57b1e855dd8d73b3900512d3c79e4bd
gayoung choi <cy614@naver.com>
2024-03-28 23:37:38 +0900

    feat : 화학식량(2)

--- Baekjoon/solution/B_2257.java
+++ Baekjoon/solution/B_2257.java
@@ -15,8 +15,21 @@ public class B_2257 {
 		for (int i = 0; i < in.length(); i++) {
 			char ch = in.charAt(i);
 			//화학식량 계산 
+			if(ch== ')') {
+				int sum = 0;
+				while(true) {
+					if(stack.peek() == 0) {
+						stack.pop(); //여는 괄호 삭제
+						stack.push(sum); //총합 추가
+						break;
+					}
+					sum += stack.pop();
+				}
+			}
+			//화학식 추가
 		}
-
+		int total = 0; //화학식량 계산값
+		
 		bw.flush();
 		bw.close();
 	}
