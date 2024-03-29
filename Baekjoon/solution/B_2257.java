6fec6930cd165fee5a92ed9531ce341d938aae8f
gayoung choi <cy614@naver.com>
2024-03-29 23:08:22 +0900

    feat : 화학식량(스택), 해결

--- Baekjoon/solution/B_2257.java
+++ Baekjoon/solution/B_2257.java
@@ -20,16 +20,28 @@ public class B_2257 {
 				while(true) {
 					if(stack.peek() == 0) {
 						stack.pop(); //여는 괄호 삭제
-						stack.push(sum); //총합 추가
+						stack.add(sum); //총합 추가
 						break;
 					}
 					sum += stack.pop();
 				}
 			}
-			//화학식 추가
+			//화학식 추가 수소, 탄소, 산소
+			else if(ch == 'H') stack.add(1);
+			else if(ch == 'C') stack.add(12);
+			else if(ch == 'O') stack.add(16);
+			//숫자
+			else if(ch >= '0' && ch <= '9') 
+				stack.add(stack.pop() * (ch - '0')); //최근값 * 현재 숫자
+			else stack.add(0);
 		}
+
 		int total = 0; //화학식량 계산값
-		
+
+		while(!stack.isEmpty()) {
+			total += stack.pop();
+		}
+		bw.write(total + "");
 		bw.flush();
 		bw.close();
 	}
