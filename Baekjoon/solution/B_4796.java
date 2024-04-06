96e97e60c6bab58734482d521fdb5255ea359262
gayoung choi <cy614@naver.com>
2024-04-06 23:02:36 +0900

    feat : 캠핑(그리디), 해결

--- Baekjoon/solution/B_4796.java
+++ Baekjoon/solution/B_4796.java
@@ -8,21 +8,24 @@ public class B_4796 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st  = new StringTokenizer(br.readLine());
-		
-		int max = 0;
+
+		int max = 1;
 		int total = 0;
-		
+
 		while(true) {
+			StringTokenizer st  = new StringTokenizer(br.readLine());
 			int L = Integer.parseInt(st.nextToken()); //캠핑장 사용가능 일수
 			int P = Integer.parseInt(st.nextToken()); //연속하는 일수
 			int V = Integer.parseInt(st.nextToken()); //휴가기간
-			
-			//캠핑장은 연속하는 P일 중 L일 동안만 사용가능
-			if(total > V) break;
-			//?? L일 중 일부만 사용하는 경우
+
+			total = L + P + V;
+			if(total == 0) break;
+
+			//이용할 수 있는 최대 기간
+			total = L * (V / P) + Math.min(L, V % P);
+			bw.write("Case " + max + ": " + total + "\n");
+			max++;
 		}
-		bw.write(max + "");
 		bw.flush();
 		bw.close();
 	}
