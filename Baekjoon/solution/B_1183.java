0a35ec7a2ed493a35723b8461059cad7c037b26a
gayoung choi <cy614@naver.com>
2024-04-16 21:00:47 +0900

    feat : 약속(수학), 해결

--- Baekjoon/solution/B_1183.java
+++ Baekjoon/solution/B_1183.java
@@ -9,19 +9,27 @@ public class B_1183 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
+
 		int N = Integer.parseInt(br.readLine());
 		int[] arr = new int[N];
-		
+
 		for (int i = 0; i < N; i++) {
 			StringTokenizer st  = new StringTokenizer(br.readLine());
 			//A - B 시간을 저장 
 			arr[i] = Integer.parseInt(st.nextToken()) - Integer.parseInt(st.nextToken());
 		}
 		Arrays.sort(arr);
-		
+
 		//기다리는 시간의 합이 최소가 되는 서로 다른 정수 T의 개수구하기
-		
+		//N이 홀수인 경우
+		if(N % 2 == 1) {
+			bw.write(1 + "\n");
+		}
+		//N이 짝수인 경우 
+		else {
+			int answer = Math.abs(arr[N/2] - arr[N/2-1]) + 1;
+			bw.write(answer + "\n");
+		}
 		bw.flush();
 		bw.close();
 	}
