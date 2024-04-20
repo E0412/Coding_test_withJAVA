03fdbfa37010f6a968bf8351e300e8210f43132b
gayoung choi <cy614@naver.com>
2024-04-20 12:34:17 +0900

    feat : 안정적인 문자열(1)

---
+++ Baekjoon/solution/B_4889.java
@@ -0,0 +1,35 @@
+package solution;
+
+import java.io.*;
+import java.util.Stack;
+
+//안정적인 문자열
+/* 빈 문자열은 안정적이다.
+ * S가 안정적이라면, {S}도 안정적인 문자열이다.
+ * S와 T가 안정적이라면, ST(두 문자열의 연결)도 안정적이다.
+ */
+public class B_4889 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int tNum = 1; //테스트 케이스 번호 
+
+		while(true) {
+			String in = br.readLine();
+			//종료조건
+			if(in.contains("-")) break;
+			char arr[] = in.toCharArray(); //문자열 변환
+			int min = 0; //최소 연산의 수 
+			Stack<Character> s = new Stack<>();
+
+			//안정적인 문자열 구하기
+			for (int i = 0; i < arr.length; i++) {
+				
+			}
+		}
+
+		bw.flush();
+		bw.close();
+	}
+}
