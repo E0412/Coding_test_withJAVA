46836d473472055a3167b47535e2f7fb9586746e
gayoung choi <cy614@naver.com>
2024-04-24 19:59:30 +0900

    feat : 벌집(1)

---
+++ Baekjoon/solution/B_2292.java
@@ -0,0 +1,23 @@
+package solution;
+
+import java.io.*;
+
+//벌집
+public class B_2292 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int N = Integer.parseInt(br.readLine());
+		int room = 1; //최소 개수의 방
+		int ran = 2; //범위
+
+		for (int i = 0; i < N; i++) {
+			if(N == 1) bw.write(1 + "");
+			//벌집의 중앙 1에서 N번 방까지 몇 개의 최소 개수의 방을 지나가는지 계산
+		}
+
+		bw.flush();
+		bw.close();
+	}
+}
