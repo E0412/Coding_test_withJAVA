1d7480d802f93267a1f20aee4282473d31c548e6
gayoung choi <cy614@naver.com>
2024-04-30 23:43:30 +0900

    feat : 극장 좌석(DP)

--- Baekjoon/solution/B_2302.java
+++ Baekjoon/solution/B_2302.java
@@ -16,9 +16,25 @@ public class B_2302 {
 			vip[i] = Integer.parseInt(br.readLine()); 
 		}
 
-		int[] sit = new int[N]; //좌석에 앉는 서로 다른 방법의 가짓수
+		int[] sit = new int[41]; //좌석에 앉는 서로 다른 방법의 가짓수
+		sit[0] = 1;
+		sit[1] = 1;
+		sit[2] = 2;
 
+		for (int i = 3; i <= N; i++) {
+			sit[i] = sit[i - 1] + sit[i - 2];
+		}
 
+		int answer = 1;
+
+		int before = 0; //이전 위치
+		for (int num : vip) {
+			answer *= sit[num - before - 1];
+			before = num;
+		}
+		answer *= sit[N - before];
+
+		bw.write(answer + "");
 		bw.flush();
 		bw.close();
 	}
