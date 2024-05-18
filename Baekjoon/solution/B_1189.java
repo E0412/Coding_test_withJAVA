b8829360fbc7c6f279d2e984cca9ef2b30bcd5eb
gayoung choi <cy614@naver.com>
2024-05-18 22:11:31 +0900

    feat : 컴백홈(DFS), 해결

--- Baekjoon/solution/B_1189.java
+++ Baekjoon/solution/B_1189.java
@@ -12,6 +12,25 @@ public class B_1189 {
 	static int[] dx = {-1, 0, 0, 1};
 	static int[] dy = {0, 1, -1, 0};
 
+	//한수가 집까지 도착하는 경우 중 거리가 K인 가짓수 구하기
+	static void DFS(int x, int y , int z) {
+		if(x == 0 && y == C - 1) { 
+			if(z == K) cnt++; //경우의 수 추가
+			return;
+		}
+
+		for (int i = 0; i < 4; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+
+			if(nx >= R || nx < 0 || ny >= C || ny < 0 || 
+					visited[nx][ny] == 1 || map[nx][ny] =='T') continue;
+			visited[nx][ny] = 1;
+			DFS(nx, ny, z + 1);
+			visited[nx][ny] = 0;
+		}
+	}
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -31,8 +50,10 @@ public class B_1189 {
 			}
 		}
 
-		//거리가 K인 가짓수를 출력
+		visited[R - 1][0] = 1;
+		DFS(R - 1, 0, 1);
 
+		bw.write(cnt + "");
 		bw.flush();
 		bw.close();
 	}
