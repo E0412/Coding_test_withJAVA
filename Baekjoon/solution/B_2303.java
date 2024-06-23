9251d92b8d86d225abd6f0653f10b64bed11a447
gayoung choi <cy614@naver.com>
2024-06-23 22:57:29 +0900

    feat : 숫자 게임(브루트포스), 해결

--- Baekjoon/solution/B_2303.java
+++ Baekjoon/solution/B_2303.java
@@ -19,11 +19,28 @@ public class B_2303 {
 			for (int j = 0; j < 5; j++) {
 				arr[j] = Integer.parseInt(st.nextToken());
 			}
+			int max = Integer.MIN_VALUE;
+
+			//세 장의 카드를 골라 합을 구한 후 일의 자리 수가 가장 큰 사람 출력
+			for (int a = 0; a < 3; a++) {
+				for (int b = a + 1; b < 4; b++) {
+					for (int c = b + 1; c < 5; c++) {
+						max = Math.max(max, (arr[a] + arr[b] + arr[c]) % 10);
+					}
+				}
+			}
+			card[i] = max;
 		}
-		int max = Integer.MIN_VALUE;
+		int idx = 0;
+		int ans = 0;
 
-		//세 장의 카드를 골라 합을 구한 후 일의 자리 수가 가장 큰 사람 출력
-
+		for (int i = 0; i < N; i++) {
+			if(ans <= card[i]) {
+				ans = card[i];
+				idx = i + 1;
+			}
+		}
+		bw.write(idx + "");
 		bw.flush();
 		bw.close();
 	}
