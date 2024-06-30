a65478a62480c8bde65d76664e625a879d6dead0
gayoung choi <cy614@naver.com>
2024-06-30 22:37:02 +0900

    feat : 베르트랑 공준(1)

---
+++ Baekjoon/solution/B_4948.java
@@ -0,0 +1,24 @@
+package solution;
+
+import java.io.*;
+
+//베르트랑 공준
+public class B_4948 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int N = Integer.parseInt(br.readLine());
+
+		while(N != 0) {
+			N = Integer.parseInt(br.readLine());
+			int cnt = 0;
+
+			//각 테스트 케이스에 대해서, n보다 크고, 2n보다 작거나 같은 소수의 개수를 출력
+
+		}
+
+		bw.flush();
+		bw.close();
+	}
+}
