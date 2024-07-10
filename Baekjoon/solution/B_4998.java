8f0c56f2437278a350a19b9a80c2277dc41b5163
gayoung choi <cy614@naver.com>
2024-07-10 22:42:53 +0900

    feat : 저금(구현)

--- Baekjoon/solution/B_4998.java
+++ Baekjoon/solution/B_4998.java
@@ -8,20 +8,23 @@ public class B_4998 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer	st = new StringTokenizer(br.readLine());
-		
-		double N = Double.parseDouble(st.nextToken());
-		double B = Double.parseDouble(st.nextToken());
-		double M = Double.parseDouble(st.nextToken());
-		
-		int cnt = 0;
-		
-		//출력이 1개만 나오는 문제 해결하기 -> 각 테스트케이스마다 반복필요
-		while(M > N) {
-			N += N * B / 100;
-			cnt++;
+
+		while(true) {
+			try {
+				StringTokenizer	st = new StringTokenizer(br.readLine());
+				double N = Double.parseDouble(st.nextToken());
+				double B = Double.parseDouble(st.nextToken());
+				double M = Double.parseDouble(st.nextToken());
+				int cnt = 0;
+				while(M > N) {
+					N += N * B / 100;
+					cnt++;
+				}
+				bw.write(cnt + "\n");
+			} catch (Exception e) {
+				break;
+			}
 		}
-		bw.write(cnt + "");
 		bw.flush();
 		bw.close();
 	}
