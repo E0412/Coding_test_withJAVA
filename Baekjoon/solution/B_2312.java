ef5d52f82d4c666a10b2fd42f738a53f272d4aed
gayoung choi <cy614@naver.com>
2024-08-01 23:55:12 +0900

    feat : 수 복원하기(1)

---
+++ Baekjoon/solution/B_2312.java
@@ -0,0 +1,29 @@
+package solution;
+
+import java.io.*;
+
+//수 복원하기
+/*
+ * 양의 정수 N이 주어졌을 때, 이 수를 소인수분해 한 결과를 출력
+ */
+public class B_2312 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int TC = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < TC; i++) {
+			int tmp = Integer.parseInt(br.readLine());
+
+			//소인수분해 
+			for (int j = 2; j <= tmp; j++) {
+				int result = 0;
+
+			}
+		}
+
+		bw.flush();
+		bw.close();
+	}
+}
