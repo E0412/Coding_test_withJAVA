3fc16be5c1f70186637415717bd69077251f476e
gayoung choi <cy614@naver.com>
2024-08-06 23:59:28 +0900

    feat : 반복수열(2)

--- Baekjoon/solution/B_2331.java
+++ Baekjoon/solution/B_2331.java
@@ -10,14 +10,30 @@ public class B_2331 {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer	st = new StringTokenizer(br.readLine());
 
-		int N = Integer.parseInt(st.nextToken());
+		String N = st.nextToken();
 		int M = Integer.parseInt(st.nextToken());
 
-		List<Integer> list = new ArrayList<Integer>();
+		List<String> list = new ArrayList<>();
 		list.add(N);
 
+		int num = 0;
 		//반복되는 부분을 제외했을 때, 수열에 남게 되는 수들의 개수를 출력
-
+		for (int i = 0; i < N.length(); i++) {
+			num += Math.pow(N.charAt(i) - '0', M);
+		}
+		
+		String next = String.valueOf(num);
+		int answer = 0;
+		if(list.contains(next)) {
+			answer = list.indexOf(next);
+			return;
+		} else {
+			list.add(String.valueOf(next));
+			
+		}
+		
+		
+		
 		bw.flush();
 		bw.close();
 	}
