5e2baac07fc7cc0c0eaeedd58016e5915459bb26
gayoung choi <cy614@naver.com>
2024-08-18 20:48:42 +0900

    feat : 시그마(수학)

--- Baekjoon/solution/B_2355.java
+++ Baekjoon/solution/B_2355.java
@@ -9,16 +9,21 @@ public class B_2355 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer	st = new StringTokenizer(br.readLine());
-		
-		long A = Integer.parseInt(st.nextToken());
-		long B = Integer.parseInt(st.nextToken());
-		long answer = 0;
-		
-		//가우스식 덧셈 (A와 B의 합) * (A부터 B까지 숫자의 개수) / 2
-		long range = (A + B) * (B - A + 1) / 2; 
-		
-		//A와 B를 포함한 두 정수 사이에 있는 수의 합 구하기
 
+		long a = Integer.parseInt(st.nextToken());
+		long b = Integer.parseInt(st.nextToken());
+
+		long A = Math.min(a, b);
+		long B = Math.max(a, b);
+
+		//가우스식 덧셈 (A와 B의 합) * (A부터 B까지 숫자의 개수) / 2
+		long result = (A + B) * ((B - A + 1) / 2); 
+
+		if((B - A + 1) % 2 != 0) {
+			result += (A + B) / 2;
+		}
+
+		bw.write(result + "");
 		bw.flush();
 		bw.close();
 	}
