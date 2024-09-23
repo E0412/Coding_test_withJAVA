dd46c9e9753b1df892e898c0c80b175e7f01de5d
gayoung choi <cy614@naver.com>
2024-09-23 22:26:46 +0900

    feat : Rook(구현)

---
+++ Baekjoon/solution/B_2393.java
@@ -0,0 +1,25 @@
+package solution;
+
+import java.io.*;
+
+public class B_2393 {
+	public static void main(String[] args) throws IOException {
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		bw.write("  ___  ___  ___\n"
+				+ "  | |__| |__| |\n"
+				+ "  |           |\n"
+				+ "   \\_________/\n"
+				+ "    \\_______/\n"
+				+ "     |     |\n"
+				+ "     |     |\n"
+				+ "     |     |\n"
+				+ "     |     |\n"
+				+ "     |_____|\n"
+				+ "  __/       \\__\n"
+				+ " /             \\\n"
+				+ "/_______________\\"
+				);
+		bw.flush();
+	}
+}
