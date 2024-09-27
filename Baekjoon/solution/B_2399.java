1796e0ce8e72921d371817daa2f9e1e537633403
gayoung choi <cy614@naver.com>
2024-09-27 22:19:48 +0900

    feat : 거리의 합(구현)

--- Baekjoon/solution/B_2399.java
+++ Baekjoon/solution/B_2399.java
@@ -10,17 +10,21 @@ public class B_2399 {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		int N = Integer.parseInt(br.readLine());
-		int[] arr = new int[N];
+		int[] x = new int[N];
 
 		StringTokenizer	st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
+			x[i] = Integer.parseInt(st.nextToken());
 		}
 
 		long answer = 0;
 		//모든쌍의 거리를 더한 값 구하기
-
-
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				answer += Math.abs(x[i] - x[j]); //|x[i] - x[j]|의 합
+			}
+		}
+		bw.write(answer + "");
 		bw.flush();
 		bw.close();
 	}
