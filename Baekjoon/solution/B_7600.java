a83ec0f8bb9179735c8898665f32e5074b9b029a
gayoung choi <cy614@naver.com>
2024-09-30 23:52:29 +0900

    feat : 문자가 몇갤까(1)

---
+++ Baekjoon/solution/B_7600.java
@@ -0,0 +1,28 @@
+package solution;
+
+import java.io.*;
+
+//문자가 몇갤까
+public class B_7600 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		while(true) {
+			String s = br.readLine();
+			
+			if(s.equals("#")) break;
+			
+			int arr[] = new int[26]; 
+			char ch[] = new char[251];
+			int cnt = 0;
+			
+			for (int i = 0; i < s.length(); i++) {
+			}
+			
+		}
+		
+		bw.flush();
+		bw.close();
+	}
+}
