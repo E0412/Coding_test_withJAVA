295910fefb0208b0f3b440ef0a640eaa2a470bbf
gayoung choi <cy614@naver.com>
2024-10-01 21:13:57 +0900

    feat : 문자가 몇갤까(문자열)

--- Baekjoon/solution/B_7600.java
+++ Baekjoon/solution/B_7600.java
@@ -7,22 +7,33 @@ public class B_7600 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
+
 		while(true) {
 			String s = br.readLine();
-			
-			if(s.equals("#")) break;
-			
-			int arr[] = new int[26]; 
-			char ch[] = new char[251];
-			int cnt = 0;
-			
-			for (int i = 0; i < s.length(); i++) {
+
+			if(s.equals("#")) {
+				break;
 			}
-			
+
+			char[] arr = s.toCharArray();
+			int cnt = 0, sum = 0, flag = 0;
+
+			for (int i = 65; i <= 90; i++) {
+				for (int j = 0; j < arr.length; j++) {
+					if((arr[j] == i) || (arr[j] == i + 32)) {
+						if(flag == 0) {
+							cnt++;
+							flag = 1;
+						}
+					}
+				}
+				flag = 0;
+				sum += cnt;
+				cnt = 0;
+			}
+			bw.write(sum + "\n");
+			bw.flush();
 		}
-		
-		bw.flush();
 		bw.close();
 	}
 }
