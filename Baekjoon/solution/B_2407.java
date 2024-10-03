52dccd2f34e774115732598575bf407705ed3be5
gayoung choi <cy614@naver.com>
2024-10-03 20:19:03 +0900

    feat : 조합(1)

---
+++ Baekjoon/solution/B_2407.java
@@ -0,0 +1,24 @@
+package solution;
+
+import java.io.*;
+
+//조합
+public class B_2407 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		String[] in = br.readLine().split(" ");
+
+		int N = Integer.parseInt(in[0]);
+		int M = Integer.parseInt(in[1]);
+
+		//nCm을 구하기
+		for (int i = 0; i < M; i++) {
+
+		}
+
+		bw.flush();
+		bw.close();
+	}
+}
