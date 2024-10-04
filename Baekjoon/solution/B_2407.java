48b00d61dc8f30279c2f39d795095a8b23853d18
gayoung choi <cy614@naver.com>
2024-10-04 20:32:44 +0900

    feat : 조합(큰 수 연산)

--- Baekjoon/solution/B_2407.java
+++ Baekjoon/solution/B_2407.java
@@ -1,6 +1,7 @@
 package solution;
 
 import java.io.*;
+import java.math.BigInteger;
 
 //조합
 public class B_2407 {
@@ -13,11 +14,17 @@ public class B_2407 {
 		int N = Integer.parseInt(in[0]);
 		int M = Integer.parseInt(in[1]);
 
+		//숫자가 커지기 때문에 BigInteger 사용
+		BigInteger a = BigInteger.ONE;
+		BigInteger b = BigInteger.ONE;
+
 		//nCm을 구하기
 		for (int i = 0; i < M; i++) {
-
+			a = a.multiply(new BigInteger(String.valueOf(N - i)));
+			b = b.multiply(new BigInteger(String.valueOf(i + 1)));
 		}
 
+		bw.write(a.divide(b) + "");
 		bw.flush();
 		bw.close();
 	}
