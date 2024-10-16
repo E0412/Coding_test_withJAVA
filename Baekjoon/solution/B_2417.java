ee42151f5ba735f58d2e1a4464b4661f2a4a4bba
gayoung choi <cy614@naver.com>
2024-10-16 22:59:23 +0900

    feat : 정수 제곱근

--- Baekjoon/solution/B_2417.java
+++ Baekjoon/solution/B_2417.java
@@ -10,10 +10,17 @@ public class B_2417 {
 		long right = in; 
 		long ans = 0;
 
+		//이분탐색
 		while(left <= right) {
-
+			long mid = (left + right) / 2;
+			//mid의 제곱이 in보다 같거나 클 때의 mid값을 ans에 저장
+			if(in <= Math.pow(mid, 2)) {
+				ans = mid;
+				right = mid - 1;
+			} else {
+				left = mid + 1;
+			}
 		}
-
 		return ans;
 	}
 
@@ -24,5 +31,7 @@ public class B_2417 {
 		long in = Long.parseLong(br.readLine());
 		long sum = binarySearch(in);
 		bw.write(sum + "");
+		bw.flush();
+		bw.close();
 	}
 }
