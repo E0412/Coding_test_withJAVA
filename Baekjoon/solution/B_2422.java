0534a10e9e892bf5181ef3d6af8aa53d30cae5cf
gayoung choi <cy614@naver.com>
2024-10-21 23:03:03 +0900

    feat : 한윤정이 이탈리아에 가서 아이스크림을 사먹는데(1)

---
+++ Baekjoon/solution/B_2422.java
@@ -0,0 +1,29 @@
+package solution;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+//한윤정이 이탈리아에 가서 아이스크림을 사먹는데 
+public class B_2422 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer	st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken()); //아이스크림 종류의 수
+		int M = Integer.parseInt(st.nextToken()); //섞어먹으면 안 되는 조합의 개수
+		int[][] select = new int[N + 1][N + 1];
+		
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int n = Integer.parseInt(st.nextToken());
+			int m = Integer.parseInt(st.nextToken());
+			select[n][m] = select[m][n] = 1; //조합
+		}
+		
+		//선택하는 방법이 몇 가지인지 구하기
+		
+		bw.flush();
+		bw.close();
+	}
+}
