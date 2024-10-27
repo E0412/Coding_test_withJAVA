bfccaf8646fb798acf77457a99dc58f24ca7fccc
gayoung choi <cy614@naver.com>
2024-10-27 22:49:03 +0900

    feat : 아즈텍 피라미드(수학)

--- Baekjoon/solution/B_7770.java
+++ Baekjoon/solution/B_7770.java
@@ -7,14 +7,25 @@ public class B_7770 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
+
 		int N = Integer.parseInt(br.readLine()); //블록의 수
-		
+
 		//블록 n개로 만들 수 있는 가장 높은 안정적인 피라미드의 높이를 출력
 		int height = 1;
 
-		//블럭끼리 맞닿아 있어야 안정적
-		
+		int block = 1; //현재 블록의 개수
+		int addBlock = 1; //추가된 블록의 개수(최소 개수)
+
+		while(true) {
+			block += 4 * height; //현재 높이-1 값을 곱한다
+			addBlock += block;
+
+			//블록의 개수를 초과할시 종료
+			if(addBlock > N) break;
+
+			height++;
+		}
+		bw.write(height + "");
 		bw.flush();
 		bw.close();
 	}
