53cfddeb8578a32211678eca198a7a78434679c3
gayoung choi <cy614@naver.com>
2024-11-05 23:05:35 +0900

    feat : 기상청 인턴 신현수(브루트포스)

--- Baekjoon/solution/B_2435.java
+++ Baekjoon/solution/B_2435.java
@@ -9,20 +9,29 @@ public class B_2435 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer	st = new StringTokenizer(br.readLine());
-		
+
 		int N = Integer.parseInt(st.nextToken());
 		int K = Integer.parseInt(st.nextToken());
 		int[] arr = new int[N];
-		
+
 		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
-		
+
 		int max = Integer.MIN_VALUE;
-		
+
 		//온도의 수열에서 연속적인 K일의 온도의 합이 최대가 되는 값 출력
-		
+		for (int i = 0; i <= N - K; i++) {
+			int cnt = 0;
+			for (int j = 0; j < K; j++) {
+				cnt += arr[i + j];
+			}
+			if(cnt > max) {
+				max = cnt;
+			}
+		}
+		bw.write(max + "");
 		bw.flush();
 		bw.close();
 	}
