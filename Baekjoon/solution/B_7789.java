051db749616e814863738fa63ee74d5141497b0c
gayoung choi <cy614@naver.com>
2024-12-16 22:21:59 +0900

    feat : 텔레프라임(소수 판정)

--- Baekjoon/solution/B_7789.java
+++ Baekjoon/solution/B_7789.java
@@ -9,17 +9,29 @@ public class B_7789 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer	st = new StringTokenizer(br.readLine());
-		
+
 		int N = Integer.parseInt(st.nextToken()); //원래 전화번호
-		int M = Integer.parseInt(st.nextToken()); //새로운 전화번호
+		int M = Integer.parseInt(st.nextToken() + N); //새로운 전화번호
+
+		int d = 2;
 		
-		if(N % 2 == 0 || N % 3 == 0) {
-			bw.write("No");
-		} else {
-			bw.write("Yes");
-		}
 		//소수 구분 
-		
+		while(true) {
+			if(d < N && N % d == 0) {
+				bw.write("No");
+				break;
+			}
+			if(d < M && M % d == 0) {
+				bw.write("No");
+				break;
+			}
+			d++; //반복 될 때 마다 1을 더한 값을 나눈다
+			
+			if(d == M) {
+				bw.write("Yes");
+				break;
+			}
+		}
 		bw.flush();
 		bw.close();
 	}
