93b31d8e7229426a32c2f4d2af22b0a76bfa6e8f
gayoung choi <cy614@naver.com>
2025-01-11 23:58:37 +0900

    feat : 주사위 네개(2)

--- Baekjoon/solution/B_2484.java
+++ Baekjoon/solution/B_2484.java
@@ -16,10 +16,35 @@ public class B_2484 {
 			StringTokenizer	st = new StringTokenizer(br.readLine());
 
 			int cnt = 0; //중복 횟수
-			int dice = 0;
-			int n_dice = 0;
+			int d1 = 0; //최대로 중복되는 주사위
+			int d2 = 0;
 
 			int[] arr = new int[4];
+			for (int a = 0; a < N; a++) {
+				arr[a] = Integer.parseInt(st.nextToken());
+
+				//1~6 반복
+				for (int b = 1; b <= 6; b++) {
+					int tmp = 0;
+
+					for (int c = 0; c < N; c++) {
+						//중복되는 경우 
+						if(b == arr[c]) {
+							tmp += tmp;
+						}
+					}
+					//큰 주사위 갯수가 많은 경우
+					if(cnt < tmp && d1 < b) {
+						cnt = tmp;
+						d1 = b;
+						d2 = 0;
+					} else if(cnt == tmp && d1 < b && cnt == 1) {
+						d1 = b;
+					} else if(cnt == tmp) {
+						d2 = b;
+					}
+				}
+			}
 		}
 
 		bw.flush();
