519778226b498cb46bc57d8acd8316a42bd05194
gayoung choi <cy614@naver.com>
2025-01-28 00:14:28 +0900

    feat : 약수 구하기(수학), 수정

--- Baekjoon/solution/B_2501.java
+++ Baekjoon/solution/B_2501.java
@@ -9,18 +9,27 @@ public class B_2501 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
+
 		//약수를 구한 뒤 배열에 저장, K번째로 작은 수 출력
+		//(실패 후 수정)-> 찾고자 하는 약수의 수를 세고 해당 약수 출력
 		int N = Integer.parseInt(st.nextToken());
 		int K = Integer.parseInt(st.nextToken());
-		int[] arr = new int[10000];
-		
+
+		int cnt = 0; //약수의 개수
+		int answer = 0;
+
 		for (int i = 1; i <= N; i++) {
+			//약수인 경우
 			if(N % i == 0) {
-				arr[i] = i;
+				cnt++; 
 			} 
+			//찾고자 하는 수 저장
+			if(cnt == K) {
+				answer = i;
+				break;
+			}
 		}
-		bw.write(arr[K] + "");
+		bw.write(answer + "");
 		bw.flush();
 		bw.close();
 	}
