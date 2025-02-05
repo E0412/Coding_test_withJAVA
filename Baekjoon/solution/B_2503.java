fa846e916e947df39c81ad9d24c5fc8df6122eee
gayoung choi <cy614@naver.com>
2025-02-05 23:55:35 +0900

    feat : 숫자 야구(실패)

--- Baekjoon/solution/B_2503.java
+++ Baekjoon/solution/B_2503.java
@@ -24,27 +24,47 @@ public class B_2503 {
 		}
 
 		int num = 0; //질문 
-		int b = 0; //볼
-		int s = 0; //스트라이크
+		int B = 0; //볼
+		int S = 0; //스트라이크
 
-		StringTokenizer st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
 			num = Integer.parseInt(st.nextToken());
-			s = Integer.parseInt(st.nextToken());
-			b = Integer.parseInt(st.nextToken());
-
-			int b_cnt = 0;
-			int s_cnt = 0;
+			S = Integer.parseInt(st.nextToken());
+			B = Integer.parseInt(st.nextToken());
 
 			//영수가 생각하고 있을 가능성이 있는 답의 총 개수 구하기
 			for (int k = 123; k <= 987; k++) {
 				//정답 조건에 해당하는 수 탐색
 				if(chk[k]) {
+					int b_cnt = 0;
+					int s_cnt = 0;
 
+					for (int a = 0; a < 3; a++) {
+						char n = Integer.toString(num).charAt(a);
+
+						for (int b = 0; b < 3; b++) {
+							char m = Integer.toString(k).charAt(b);
+
+							if(n == m && a == b) {
+								s_cnt++;
+							} else if(n == m && a != b) {
+								b_cnt++;
+							}
+						}
+						if(s_cnt == S && b_cnt == B) chk[k] = true;
+						else chk[k] = false;
+					}
 				}
 			}
 		}
 
+		int answer = 0;
+		for (int i = 123; i <= 987; i++) {
+			if(chk[i]) answer++;
+		}
+
+		bw.write(answer + "");
 		bw.flush();
 		bw.close();
 	}
