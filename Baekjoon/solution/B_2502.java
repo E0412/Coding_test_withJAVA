36271b6561826d48d3a53697ec5d6b48f8ce41fb
gayoung choi <cy614@naver.com>
2025-02-21 23:58:17 +0900

    feat : 떡 먹는 호랑이(2)

--- Baekjoon/solution/B_2502.java
+++ Baekjoon/solution/B_2502.java
@@ -13,14 +13,22 @@ public class B_2502 {
 		int D = Integer.parseInt(st.nextToken()); //할머니가 넘어온 날
 		int K = Integer.parseInt(st.nextToken()); //호랑이에게 준 떡의 개수
 
-		int A = 0; //첫 날에 준 떡의 개수 
-		int B = 0; //둘째 날에 준 떡의 개수
+		int[] arr = new int[D];
 
-		//첫줄에 첫 날에 준 떡의 개수 A를 출력하고 그 다음 둘째 줄에는 둘째 날에 준 떡의 개수 B를 출력
-
-
-
-		bw.flush();
-		bw.close();
+		for (int a = 1; a <= K / 2; a++) {
+			for (int b = a + 1; b < K; b++) {
+				arr[0] = a;
+				arr[1] = b;
+				for (int c = 2; c < D; c++) {
+					arr[c] = arr[c-1] + arr[c-2];
+				}
+				if(arr[D-1] == K) {
+					bw.write(arr[0] + "\n");
+					bw.write(arr[1] + "\n");
+					bw.flush();
+					bw.close();
+				}
+			}
+		}
 	}
 }
