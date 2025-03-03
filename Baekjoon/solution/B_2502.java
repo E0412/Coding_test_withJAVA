2563e731898e793d0505a2339aa42933a57b84df
gayoung choi <cy614@naver.com>
2025-03-03 23:14:33 +0900

    feat : 떡 먹는 호랑이(dp)

--- Baekjoon/solution/B_2502.java
+++ Baekjoon/solution/B_2502.java
@@ -13,22 +13,25 @@ public class B_2502 {
 		int D = Integer.parseInt(st.nextToken()); //할머니가 넘어온 날
 		int K = Integer.parseInt(st.nextToken()); //호랑이에게 준 떡의 개수
 
-		int[] arr = new int[D];
-
-		for (int a = 1; a <= K / 2; a++) {
-			for (int b = a + 1; b < K; b++) {
-				arr[0] = a;
-				arr[1] = b;
-				for (int c = 2; c < D; c++) {
-					arr[c] = arr[c-1] + arr[c-2];
-				}
-				if(arr[D-1] == K) {
-					bw.write(arr[0] + "\n");
-					bw.write(arr[1] + "\n");
-					bw.flush();
-					bw.close();
-				}
+		int[] a = new int[D];
+		int[] b = new int[D];
+		
+		a[0] = 1; a[1] = 0;
+		b[0] = 0; b[1] = 1;
+		
+		for (int i = 2; i < D; i++) {
+			a[i] = a[i-2] + a[i-1];
+			b[i] = b[i-2] + b[i-1];
+		}
+		for (int i = 1; i < K; i++) {
+			int cnt = K - i * a[D-1];
+			if(cnt % b[D-1] == 0) {
+				bw.write(i + "\n");
+				bw.write(cnt / b[D-1] + "");
+				break;
 			}
 		}
+		bw.flush();
+		bw.close();
 	}
 }
