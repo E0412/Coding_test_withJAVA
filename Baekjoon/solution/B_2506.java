e592b2bfd3044a671083077ee02160826e94e19a
gayoung choi <cy614@naver.com>
2025-03-11 23:08:16 +0900

    feat : 점수계산(구현)

--- Baekjoon/solution/B_2506.java
+++ Baekjoon/solution/B_2506.java
@@ -8,29 +8,37 @@ public class B_2506 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
+
 		int N = Integer.parseInt(br.readLine());
-		int cnt = 0;
 		int[] arr = new int[N];
-		
+
+		int cnt = 0;
+		int answer = 0;
+		boolean chk = false;
+
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 
+			//문제를 맞춘 경우 
 			if(arr[i] == 1) {
-				cnt++;
-			} 
-			//연속으로 문제의 답이 맞는 경우
-			//배열 초과가 발생. 다른 방법 찾기
-			if(arr[i + 1] == 1) {
-				cnt = cnt + 1;
+				//이전 문제를 맞은 경우
+				if(chk) {
+					cnt++;
+				}
+				//이전 문제가 틀린경우
+				else {
+					cnt = 1; //초기화
+					chk = true;
+				} 
+				answer += cnt;
+			} else {
+				chk = false;
+				cnt = 0; //초기화 
 			}
-			if(arr[i + 2] == 1) {
-				cnt = cnt + 2;
-			}
-			bw.write(cnt + "");
-			bw.flush();
 		}
+		bw.write(answer + "");
+		bw.flush();
 		bw.close();
 	}
 }
