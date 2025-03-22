737043679ac425150a0aa3cbfb0a96dd5c8d2d89
gayoung choi <cy614@naver.com>
2025-03-22 22:58:33 +0900

    feat : 이진수의 합(1)

---
+++ Baekjoon/solution/B_8741.java
@@ -0,0 +1,22 @@
+package solution;
+
+import java.io.*;
+
+//이진수 합
+public class B_8741 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int K = Integer.parseInt(br.readLine());
+		int sum = 0;
+		
+		//1.이진수로 나타내었을 때 k자리 이하인 자연수 구하기
+		//2.해당 자연수의 합을 구하여 이진수로 출력
+		
+		
+		
+		bw.flush();
+		bw.close();
+	}
+}
