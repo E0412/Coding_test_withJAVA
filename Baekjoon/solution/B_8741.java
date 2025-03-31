1f34392b60a87fd1dae707cf4e47e3b886729ac2
gayoung choi <cy614@naver.com>
2025-03-31 22:59:50 +0900

    feat : 이진수 합(구현)

--- Baekjoon/solution/B_8741.java
+++ Baekjoon/solution/B_8741.java
@@ -7,15 +7,20 @@ public class B_8741 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
 
 		int K = Integer.parseInt(br.readLine());
 		int sum = 0;
-		
-		//1.이진수로 나타내었을 때 k자리 이하인 자연수 구하기
-		//2.해당 자연수의 합을 구하여 이진수로 출력
-		
-		
-		
+
+		// K인 경우 1이 K개 0이 K - 1개임에 따라 1과 0을 저장
+		for (int i = 0; i < K; i++) {
+			sb.append("1");	
+		}
+
+		for (int i = 0; i < K - 1; i++) {
+			sb.append("0");	
+		}
+		bw.write(sb.toString());
 		bw.flush();
 		bw.close();
 	}
