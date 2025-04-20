6043c3c6ecbc76b3bb3357a46f83c5940bbc373b
gayoung choi <cy614@naver.com>
2025-04-20 23:54:05 +0900

    feat : 예산(이분 탐색)

--- Baekjoon/solution/B_2512.java
+++ Baekjoon/solution/B_2512.java
@@ -11,16 +11,35 @@ public class B_2512 {
 
 		int N = Integer.parseInt(br.readLine()); //지방의 수 
 		int arr[] = new int[N];
+		int left = 0;
+		int right = -1;
 
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken()); 
+			right = Math.max(right, arr[i]);
 		}
 
 		int M = Integer.parseInt(br.readLine()); //전체 예산
 
 		//배정된 예산들 중 최댓값인 정수를 출력
-
+		while(left <= right) {
+			int mid = (left + right) / 2;
+			long num = 0;
+			for (int i = 0; i < N; i++) {
+				if(arr[i] > mid) {
+					num += mid;
+				} else {
+					num += arr[i];
+				}
+			}
+			if(num <= M) {
+				left = mid + 1;
+			} else {
+				right = mid - 1;
+			}
+		}
+		bw.write(right + "");
 		bw.flush();
 		bw.close();
 	}
