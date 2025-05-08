b9e17363b88e26d0130260ca6d1858fdbf64ebea
gayoung choi <cy614@naver.com>
2025-05-08 23:06:04 +0900

    feat : 인공지능 시계(수학)

--- Baekjoon/solution/B_2530.java
+++ Baekjoon/solution/B_2530.java
@@ -9,14 +9,21 @@ public class B_2530 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
+
 		int A = Integer.parseInt(st.nextToken()); //시
 		int B = Integer.parseInt(st.nextToken()); //분
 		int C = Integer.parseInt(st.nextToken()); //초
-		int D = Integer.parseInt(st.nextToken()); //요리하는데 필요한 시간
-		
-		//종료되는 시각의 시, 분, 초를 출력
-		
+
+		int D = Integer.parseInt(br.readLine()); //요리하는데 필요한 시간
+
+		C += D; //요리하는데 필요한 시간을 더한다
+		B += (C / 60); //C의 값이 60이 넘으면 B에 더한다
+		C %= 60; 
+		A += (B / 60); //B의 값이 60이 넘으면 A에 더한다
+		B %= 60;
+		A %= 24; //24시가 넘는 경우 0시가 되어야 함 
+
+		bw.write(A + " " + B + " " + C);
 		bw.flush();
 		bw.close();
 	}
