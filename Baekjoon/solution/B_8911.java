5ffdb64bb2b501c8e5281e30514d04d34fcffa82
gayoung choi <cy614@naver.com>
2025-05-13 23:54:50 +0900

    feat : 거북이(1)

---
+++ Baekjoon/solution/B_8911.java
@@ -0,0 +1,35 @@
+package solution;
+
+import java.io.*;
+
+//거북이
+public class B_8911 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int N = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < N; i++) {
+			String s = br.readLine();
+
+			//한 눈금 앞으로
+			if(s.equals("F")) {
+
+			} 
+			//한 눈금 뒤로
+			else if(s.equals("B")) {
+
+			}
+			//왼쪽으로 90도 회전
+			else if(s.equals("L")) {
+
+			}
+			//오른쪽으로 90도 회전
+			else if(s.equals("R")) {
+
+			}
+		}
+
+	}
+}
