b487e543b464adab7ddb8228c7aba3154eb8c620
gayoung choi <cy614@naver.com>
2025-05-28 22:30:04 +0900

    feat : 부등호(2)

--- Baekjoon/solution/B_2529.java
+++ Baekjoon/solution/B_2529.java
@@ -1,23 +1,46 @@
 package solution;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 //부등호
 public class B_2529 {
+
+	static int K;
+	static char[] arr;
+	static boolean visited[] = new boolean[10];
+	static ArrayList<String> ans = new ArrayList<String>();
+
+
+	static void DFS(int p, int cnt, String num) {
+		if(cnt == K) {
+			ans.add(num);
+			return;
+		}
+
+		//탐색
+	}
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		int K = Integer.parseInt(br.readLine());
-		char arr[] = new char[K];
+		K = Integer.parseInt(br.readLine());
+		arr = new char[K];
 
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < K; i++) {
 			arr[i] = st.nextToken().charAt(0);
 		}
 
+		for (int i = 0; i < 10; i++) {
+			visited[i] = true;
+			DFS(i, 0, i + ""); //i + "" -> 문자열로 변경
+			visited[i] = false; 
+		}
 
+		
 		bw.flush();
 		bw.close();
 	}	
