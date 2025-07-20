e346e98341aa33ebd9ad02649f7200f15635d906
gayoung choi <cy614@naver.com>
2025-07-20 14:03:24 +0900

    feat : 사탕 선생 고창영(큰 수 연산)

--- Baekjoon/solution/B_2547.java
+++ Baekjoon/solution/B_2547.java
@@ -1,6 +1,7 @@
 package solution;
 
 import java.io.*;
+import java.math.BigInteger;
 
 //사탕 선생 고창영
 public class B_2547 {
@@ -12,16 +13,19 @@ public class B_2547 {
 
 		for (int i = 0; i < T; i++) {
 			br.readLine();
+			//NumberFormat 에러가 발생 -> BigInteger로 타입을 바꾸어 해결
+			BigInteger sum = new BigInteger("0"); //BigInteger 생성
 			int N = Integer.parseInt(br.readLine()); //학생 수
-			long sum = 0;
 
 			for (int j = 0; j < N; j++) {
-				int candy = Integer.parseInt(br.readLine()); //학생 수
-				sum = (sum + candy) % N;
+				sum = sum.add(BigInteger.valueOf(Long.parseLong(br.readLine())));
 			}
-			if(sum % N == 0) {
+			//총합을 N으로 나눈 나머지가 0이면 YES 출력
+			if(sum.mod(BigInteger.valueOf(N)).equals(BigInteger.ZERO)) {
 				bw.write("YES" + "\n");
-			} else {
+			} 
+			//아닐시 NO 출력
+			else {
 				bw.write("NO" + "\n");
 			}
 		}
