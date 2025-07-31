a8853541b71e450b029eb32c27be08cdb2fcf1ba
gayoung choi <cy614@naver.com>
2025-07-31 15:39:24 +0900

    feat : PG0 - 문자열 출력하기

---
+++ Programmers/level_0/L0_01.java
@@ -0,0 +1,12 @@
+package level_0;
+
+import java.io.*;
+
+//기초 트레이닝 - 문자열 출력하기
+public class L0_01 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();
+		System.out.println(s);
+	}
+}
