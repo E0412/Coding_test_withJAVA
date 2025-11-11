0ca20e1a6a517d7a760b21908b382e65f7397552
gayoung choi <cy614@naver.com>
2025-11-11 23:11:07 +0900

    feat : PG0 - 배열 비교하기

---
+++ Programmers/level_0/L0_74.java
@@ -0,0 +1,30 @@
+package level_0;
+
+//배열 비교하기
+public class L0_74 {
+	public int solution(int[] arr1, int[] arr2) {
+		int sum1 = 0;
+		int sum2 = 0;
+
+		int answer = 0;
+		if(arr1.length > arr2.length) {
+			answer = 1;
+		} else if(arr1.length < arr2.length) {
+			answer = -1;
+		} 
+		else if(arr1.length == arr2.length) {
+			for (int i = 0; i < arr2.length; i++) {
+				sum1 += arr1[i];
+				sum2 += arr2[i];
+			}
+			if(sum1 > sum2) {
+				answer = 1;
+			} else if(sum1 < sum2) {
+				answer = -1;
+			} else if(sum1 == sum2) {
+				answer = 0;
+			}
+		}
+		return answer;
+	}
+}
