0b52c5845598daf48ae13135725ebf4dbc5a10b8
gayoung choi <cy614@naver.com>
2026-02-23 23:54:43 +0900

    feat : PG1 - 문자열 다루기 기본

---
+++ Programmers/level_1/L1_22.java
@@ -0,0 +1,16 @@
+package level_1;
+
+//문자열 다루기 기본
+public class L1_22 {
+	public boolean solution(String s) {
+		if(s.length() != 4 && s.length() != 6) {
+			return false;
+		}
+		for(int i = 0; i < s.length(); i++) {
+			if(s.charAt(i) < '0' || s.charAt(i) > '9') {
+				return false;
+			}
+		}
+		return true;
+	}
+}
