229e59840315fbbb1346f9b7b2864e0f4db6e13b
gayoung choi <cy614@naver.com>
2026-03-13 23:56:33 +0900

    feat : PG0 - 외계어 사전(해결)

--- Programmers/level_0/L0_150.java
+++ Programmers/level_0/L0_150.java
@@ -3,19 +3,20 @@ package level_0;
 //외계어 사전
 public class L0_150 {
 	public int solution(String[] spell, String[] dic) {
-		int answer = 1;
-		int cnt = 0;
+		int answer = 2;
 		for(int i = 0; i < dic.length; i++) {
-			for(int j = 0; j < spell.length; j++) {
-				if(spell[j].equals(dic[i])) {
-					cnt++;
-				} 
-				if(cnt == spell.length) {
-					answer = 2;
+			if(dic[i].length() == spell.length) {
+				boolean chk = true;
+				for(int j = 0; j < spell.length; j++) {
+					if(!dic[i].contains(spell[j])) {
+						chk = false;
+					}
+				}
+				if(chk) {
+					answer = 1;
 					break;
 				}
 			}
-			cnt = 0;
 		}
 		return answer;
 	}
