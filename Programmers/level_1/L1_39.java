6585bd6e3567b6fa7ac9970d4542f98a30009884
gayoung choi <cy614@naver.com>
2026-04-06 23:59:52 +0900

    feat : PG1 - K번째 수(1)

---
+++ Programmers/level_1/L1_39.java
@@ -0,0 +1,20 @@
+package level_1;
+
+import java.util.*;
+
+//K번째 수
+public class L1_39 {
+	public int[] solution(int[] array, int[][] commands) {
+        List<Integer> answer = new ArrayList<>();
+        
+        for(int i = 0; i < commands.length; i++) {
+            List<Integer> list = new ArrayList<>();
+            for(int j = commands[i][0] - 1; j < commands[i][1] - 1; j++) {
+                list.add(array[j]);
+            }
+            Collections.sort(list);
+            answer.add(list.get(commands[i][2] - 1));
+        }
+        return answer.stream().mapToInt(i -> i).toArray();
+    }
+}
