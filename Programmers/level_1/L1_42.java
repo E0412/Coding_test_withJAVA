051c48bd575e8b3edfd1878c64da1c77c5075b3f
gayoung choi <cy614@naver.com>
2026-04-19 13:43:32 +0900

    feat : PG1 - 가장 가까운 같은 글자(해결)

--- Programmers/level_1/L1_42.java
+++ Programmers/level_1/L1_42.java
@@ -4,10 +4,13 @@ package level_1;
 public class L1_42 {
 	public int[] solution(String s) {
 		int[] answer = new int[s.length()];
+		answer[0] = -1; //첫번째 인덱스는 -1
 		for (int i = 0; i < s.length(); i++) {
-			for (int j = i + 1; j < s.length(); j++) {
+			//반복문 역순으로 수정
+			for (int j = i - 1; j >= 0; j--) {
 				if(s.charAt(i) == s.charAt(j)) {
-					answer[i] = j - i;
+					answer[i] = i - j;
+					break;
 				} else {
 					answer[i] = -1;
 				}
