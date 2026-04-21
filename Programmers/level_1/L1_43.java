d563da419a07629379554447f5ca1aa48fbecc28
gayoung choi <cy614@naver.com>
2026-04-21 23:43:59 +0900

    feat : PG1 - 콜라 문제(해결)

--- Programmers/level_1/L1_43.java
+++ Programmers/level_1/L1_43.java
@@ -4,16 +4,13 @@ package level_1;
 public class L1_43 {
 	public int solution(int a, int b, int n) {
 		int answer = 0;
-		//실패 : 1개가 모자란 결과가 나옴 
-		while(n > 0) {
-			if(n % a == 0) {
-				n /= a;
-				answer += n * b;
-			} else {
-				n /= a;
-				answer += n * b;
-				n += (n % a);
-			}
+
+		while(n >= a) {
+			int cnt = n / a; //교환 가능한 횟수
+			int remain = n % a; //남은 병의 수
+
+			answer += cnt * b;
+			n = cnt * b + remain; //새 병 + 남은 병
 		}
 		return answer;
 	}
