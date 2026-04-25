bb72192ff5ed7bda05d1353806c09a5d9b3da936
gayoung choi <cy614@naver.com>
2026-04-25 23:34:42 +0900

    feat : PG1 - 명예의 전당 (1)

--- Programmers/level_1/L1_44.java
+++ Programmers/level_1/L1_44.java
@@ -8,13 +8,10 @@ public class L1_44 {
 		List<Integer> list = new ArrayList<>();
 		List<Integer> answer = new ArrayList<>();
 		for(int i = 1; i < score.length; i++) {
-			if(list.size() < 4) {
-				list.add(score[i-1]);
-			} else {
-				list.add(Math.max(score[i-1], score[i]));
-			}
+			list.add(score[i]);
 			Collections.sort(list);
-			if(list.size() > 3) {
+
+			if(list.size() > k) {
 				list.remove(0);
 			}
 			answer.add(list.get(0));      
