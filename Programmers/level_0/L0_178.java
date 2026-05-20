0cc085b11e3a58f9a91a53dd9ad710849ff7aaec
gayoung choi <cy614@naver.com>
2026-05-20 23:46:23 +0900

    modify : PG0 - 다음에 올 숫자(해결)

--- Programmers/level_0/L0_178.java
+++ Programmers/level_0/L0_178.java
@@ -4,14 +4,13 @@ package level_0;
 public class L0_178 {
 	public int solution(int[] common) {
 		int answer = 0;
-		int n = common[1] - common[0];
-		int m = common[1] / common[0];
 
-		//배열 크기가 2이하인 경우 오류 발생, 수정 필요
-		if(common[1] * m == common[2]) {
-			answer = common[common.length-1] * m;
-		} else if(common[1] + n == common[2]) {
-			answer = common[common.length-1] + n;
+		//런타임 에러 발생 -> 인덱스간 차이를 비교
+		if(common[1] - common[0] == common[2] - common[1]) {
+			answer = common[common.length-1] + (common[1] - common[0]);
+		} //등차 먼저 검사
+		else if(common[1] / common[0] == common[2] / common[1]) {
+			answer = common[common.length-1] * (common[1] / common[0]);
 		}
 		return answer;
 	}
