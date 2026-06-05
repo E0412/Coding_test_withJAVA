c0ab7726bb2735fef93985944efd8f26f0241495
gayoung choi <cy614@naver.com>
2026-06-05 23:38:35 +0900

    modify : PG2 - H-Index(해결)

--- Programmers/level_2/L2_20.java
+++ Programmers/level_2/L2_20.java
@@ -11,13 +11,11 @@ public class L2_20 {
 
 		//citations[i]번 이상 인용됐는지 확인, 횟수가 인용된 수와 동일한지 확인
 		for(int i = 0; i < citations.length; i++) {
-			answer = 0;
-			for(int j = i; j < citations.length; j++) {
-				if(citations[i] <= citations[j]) {
-					answer++;
-				}
+			//전체 논문 수 - i와 비교하여 해당하는 가장 작은 값 출력
+			if(citations[i] >= citations.length - i) {
+				answer = citations.length - i; //논문 수 저장
+				break;
 			}
-			if(answer == citations[i]) break;
 		}
 		return answer;
 	}
