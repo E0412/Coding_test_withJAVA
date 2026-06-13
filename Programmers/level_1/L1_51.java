6dcf21548bee6ae6618ac87b29aa95cdd24e310a
gayoung choi <cy614@naver.com>
2026-06-13 23:55:14 +0900

    modify : PG1 - 모의고사(해결)

--- Programmers/level_1/L1_51.java
+++ Programmers/level_1/L1_51.java
@@ -10,9 +10,27 @@ public class L1_51 {
 		int[] two = {2, 1, 2, 3, 2, 4, 2, 5};
 		int[] three = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
 
-		for(int i = 0; i < answers.length; i++) {
+		int score1 = 0;
+		int score2 = 0;
+		int score3 = 0;
 
+		for(int i = 0; i < answers.length; i++) {
+			//숫자가 같으면 score++
+			if(answers[i] == one[i % one.length]) {
+				score1++;
+			}
+			if(answers[i] == two[i % two.length]) {
+				score2++;
+			}
+			if(answers[i] == three[i % three.length]) {
+				score3++;
+			}
 		}
+		int max = Math.max(score1, Math.max(score2, score3));
+
+		if(score1 == max) list.add(1);
+		if(score2 == max) list.add(2);
+		if(score3 == max) list.add(3);
 
 		return list.stream().mapToInt(i -> i).toArray();
 	}
