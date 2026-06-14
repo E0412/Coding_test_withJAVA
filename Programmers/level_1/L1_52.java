87c564294e0309d34e39d6d3ecac59e383233033
gayoung choi <cy614@naver.com>
2026-06-14 17:00:38 +0900

    feat : PG1 - 과일 장수(시도 1)

---
+++ Programmers/level_1/L1_52.java
@@ -0,0 +1,25 @@
+package level_1;
+
+import java.util.Arrays;
+
+//과일 장수
+public class L1_52 {
+	public int solution(int k, int m, int[] score) {
+		int answer = 0;
+		//내림차순 정렬
+		Arrays.sort(score);
+
+		int len = score.length;
+		/*  m개씩 끊기
+        	각 묶음의 마지막 원소(최저점수) 찾기
+        	최저점수 x m
+		 */
+		for(int i = 0; i < score.length; i++) {
+			if(len - m >= m) {
+				answer += (score[len - m] * m);
+			}
+		}
+
+		return answer;
+	}
+}
