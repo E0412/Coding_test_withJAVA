e0f07ea5da40ac5d9dd1bea77184f7a181f3b1a4
gayoung choi <cy614@naver.com>
2026-06-18 01:06:55 +0900

    modify : PG1 - 옹알이 (2) (해결)

--- Programmers/level_1/L1_53.java
+++ Programmers/level_1/L1_53.java
@@ -6,26 +6,32 @@ public class L1_53 {
 		int answer = 0;
 		String[] arr = {"aya", "ye", "woo", "ma"};
 
-		for(int i = 0; i < babbling.length; i++) {
-			String s = babbling[i];
-			boolean chk = true;
 
-			//같은 단어가 반복되는지 검증
-			for (String re : arr) {
-				if(s.contains(re + re)) {
+		//modify : 순서에 따라서 단어를 삭제하는 방식 사용
+		for (int i = 0; i < babbling.length; i++) {
+			String s = babbling[i];
+			int idx = 0; 
+			String prev = ""; //이전값 저장
+			boolean chk = true; //단어가 순서대로 있는지
+
+			while(idx < s.length()) {
+				boolean found = false; //해당하는 단어가 있는지 확인
+
+				for(String str : arr) {
+					//idx로 시작하는 문자열이 str로 시작하고 직전 단어와 다른경우
+					if(s.substring(idx).startsWith(str) && !prev.equals(str)) {
+						idx += str.length(); //단어 길이만큼 idx 이동 
+						prev = str; //직전에 읽은 단어를 저장
+						found = true; //읽을 수 있는 단어를 찾음
+						break;
+					}
+				}
+				if(!found) {
 					chk = false;
 					break;
 				}
 			}
-			//발음 가능한 문자열들 다 지웠을 때 빈 문자열이 되는지 검증
-			for(int j = 0; j < arr.length; j++) {
-				s = s.replace(arr[j], "");
-			}
-
-			//반복되는 같은 단어가 없으면서 빈 문자열
-			if(chk && s.isEmpty()) {
-				answer++;
-			}
+			if(chk && idx == s.length()) answer++;
 		}
 		return answer;
 	}
