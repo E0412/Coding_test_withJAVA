934a3d798e2cb7027bfbf8e7a08c4c3bed15e983
gayoung choi <cy614@naver.com>
2026-06-17 23:46:51 +0900

    modify : PG1 - 옹알이 (2) (시도 2)

--- Programmers/level_1/L1_53.java
+++ Programmers/level_1/L1_53.java
@@ -7,11 +7,24 @@ public class L1_53 {
 		String[] arr = {"aya", "ye", "woo", "ma"};
 
 		for(int i = 0; i < babbling.length; i++) {
-			for(int j = 0; j < arr.length; j++) {
-				//발음끼리 합쳐서 검증 필요
-				if(babbling[i].contains(arr[j])) {
-					answer++;
+			String s = babbling[i];
+			boolean chk = true;
+
+			//같은 단어가 반복되는지 검증
+			for (String re : arr) {
+				if(s.contains(re + re)) {
+					chk = false;
+					break;
 				}
+			}
+			//발음 가능한 문자열들 다 지웠을 때 빈 문자열이 되는지 검증
+			for(int j = 0; j < arr.length; j++) {
+				s = s.replace(arr[j], "");
+			}
+
+			//반복되는 같은 단어가 없으면서 빈 문자열
+			if(chk && s.isEmpty()) {
+				answer++;
 			}
 		}
 		return answer;
