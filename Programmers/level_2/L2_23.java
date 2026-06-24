d8a4984c94983a1f4e51be49b98ca0ddf87a4a70
gayoung choi <cy614@naver.com>
2026-06-24 23:47:34 +0900

    modify : PG2 - 행렬의 곱셈(해결)

--- Programmers/level_2/L2_23.java
+++ Programmers/level_2/L2_23.java
@@ -3,17 +3,18 @@ package level_2;
 //행렬의 곱셈
 public class L2_23 {
 	public int[][] solution(int[][] arr1, int[][] arr2) {
-		int[][] answer = new int[arr1.length][arr2.length];
+		int[][] answer = new int[arr1.length][arr2[0].length];
 
 		/* 행렬 곱
 		 * arr1[0][0] * arr2[0][0] + arr1[0][1] * arr2[1][0]
 		 */
 		for (int i = 0; i < arr1.length; i++) {
-			for (int j = 0; j < arr2.length; j++) {
-				answer[i][j] = arr1[i][j] * arr2[i][j] + arr1[i][j + 1] * arr2[i + 1][j];  
+			for (int j = 0; j < arr2[0].length; j++) {
+				for (int k = 0; k < arr1[0].length; k++) {
+					answer[i][j] += arr1[i][k] * arr2[k][j]; 
+				}
 			}
 		}
-
 		return answer;
 	}
 }
