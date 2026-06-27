bb80c78981c37719ab30260605250ad597a660cd
gayoung choi <cy614@naver.com>
2026-06-27 23:44:55 +0900

    modify : PG1 - 소수 만들기(해결)

--- Programmers/level_1/L1_55.java
+++ Programmers/level_1/L1_55.java
@@ -3,7 +3,8 @@ package level_1;
 //소수 만들기
 public class L1_55 {
 	boolean chk(int n) {
-		for (int i = 0; i * i <= n; i++) {
+		if(n < 2) return false;
+		for (int i = 2; i * i <= n; i++) { //i는 2부터 시작
 			if(n % i == 0) {
 				return false;
 			}
@@ -12,14 +13,18 @@ public class L1_55 {
 	}
 
 	public int solution(int[] nums) {
-		int answer = -1;
-
-		int lt = 0;
-		int rt = lt + 1;
+		int answer = 0;
 
 		//경우의 수 + 소수 구하기
 		for (int i = 0; i < nums.length; i++) {
-
+			for (int j = i + 1; j < nums.length; j++) {
+				for (int k = j + 1; k < nums.length; k++) {
+					int sum = nums[i] + nums[j] + nums[k];
+					if(chk(sum)) {
+						answer++;
+					}
+				}
+			}
 		}
 		return answer;
 	}
