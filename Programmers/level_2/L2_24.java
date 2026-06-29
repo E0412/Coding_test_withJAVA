00c7d1a77e24ff479b2981b1938f9ba90a1cc660
gayoung choi <cy614@naver.com>
2026-06-29 23:41:37 +0900

    feat : PG2 - n^2 배열 자르기(해결)

--- Programmers/level_2/L2_24.java
+++ Programmers/level_2/L2_24.java
@@ -1,27 +1,17 @@
 package level_2;
 
-import java.util.*;
-
 //n^2 배열 자르기
 public class L2_24 {
 	public int[] solution(int n, long left, long right) {
-		int idx = (int) ((int) right - left) + 1;
-		int[] answer = new int[idx];
+		int[] answer = new int[(int) (right - left) + 1];
+		int idx = 0;
 
-		int[][] arr = new int[n][n];
-		List<Integer> list = new ArrayList<>();
+		for (long i = left; i <= right; i++) {
+			long row = i / n;
+			long col = i % n;
 
-		//메모리 초과, 다른 방식 찾기
-		for (int i = 0; i < arr.length; i++) {
-			for (int j = 0; j < arr.length; j++) {
-				arr[i][j] = Math.max(i, j) + 1; 
-				list.add(arr[i][j]);
-			}
-		}
-
-		int m = 0;
-		for(long i = left; i <= right; i++) {
-			answer[m++] = list.get((int)i);
+			answer[idx] = (int) Math.max(row, col) + 1;
+			idx++;
 		}
 		return answer;
 	}
