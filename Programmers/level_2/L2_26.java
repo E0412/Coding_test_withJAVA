8f055742e268d6310644ab981285d39df3218235
gayoung choi <cy614@naver.com>
2026-07-04 23:35:10 +0900

    modify : PG2 - 영어 끝말잇기(해결)

--- Programmers/level_2/L2_26.java
+++ Programmers/level_2/L2_26.java
@@ -1,22 +1,30 @@
 package level_2;
 
+import java.util.*;
+
 //영어 끝말잇기
 public class L2_26 {
 	public int[] solution(int n, String[] words) {
 		int[] answer = new int[2]; //가장 먼저 탈락하는 번호, 탈락 순서
 
-		int num = 2;
-		for(int i = 1; i < words.length; i++) {
-			String s = String.valueOf(words[i - 1].charAt(words[i - 1].length() - 1));
-			if(words[i].startsWith(s)) {
-				num++;
-				//이전에 나온 단어인지 검증 필요
-			}
-			if(num > 3) {
+		int num = 0; //탈락하는 번호
+		int round = 0; //회차
 
+		Set<String> set = new HashSet<>();
+		set.add(words[0]);
+
+		for(int i = 1; i < words.length; i++) {
+			String first = String.valueOf(words[i].charAt(0));
+			String last = String.valueOf(words[i - 1].charAt(words[i - 1].length() - 1));
+
+			if(set.contains(words[i]) || !last.equals(first)) {
+				num = i % n + 1;
+				round = i / n + 1;
+				return new int[]{num, round};
+			} else if(last.equals(first)){
+				set.add(words[i]);
 			}
 		}
-
 		return answer;
 	}
 }
