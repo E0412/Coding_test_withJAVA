d07e05cee3dd66ee0f70811a49395f68d497015e
gayoung choi <cy614@naver.com>
2026-07-15 23:50:03 +0900

    feat : PG2 - 모음사전 (시도 1)

---
+++ Programmers/level_2/L2_30.java
@@ -0,0 +1,17 @@
+package level_2;
+
+//모음사전
+public class L2_30 {
+	public int solution(String word) {
+        int answer = 0;
+        
+//        String[] arr = {"A", "E", "I", "O", "U"};
+        char[] ch = word.toCharArray();
+        
+        for (int i = 0; i < ch.length; i++) {
+			
+		}
+        
+        return answer;
+    }
+}
