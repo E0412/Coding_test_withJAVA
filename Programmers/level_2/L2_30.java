3bac8d36ce43d8fa04bfe93665b50eb9b9f8d442
gayoung choi <cy614@naver.com>
2026-07-20 23:58:04 +0900

    modify : PG2 - 모음사전

--- Programmers/level_2/L2_30.java
+++ Programmers/level_2/L2_30.java
@@ -3,15 +3,20 @@ package level_2;
 //모음사전
 public class L2_30 {
 	public int solution(String word) {
-        int answer = 0;
-        
-//        String[] arr = {"A", "E", "I", "O", "U"};
-        char[] ch = word.toCharArray();
-        
-        for (int i = 0; i < ch.length; i++) {
-			
+		int answer = 0;
+
+		char[] ch = word.toCharArray();
+		int[] arr = {781, 156, 31, 6, 1}; //가중치
+		char[] cArr = {'A', 'E', 'I', 'O', 'U'};
+
+		for (int i = 0; i < ch.length; i++) {
+			for (int j = 0; j < cArr.length; j++) {
+				if(ch[i] == cArr[j]) {
+					answer += (j * arr[i]) + 1;
+				}
+			}
 		}
-        
-        return answer;
-    }
+
+		return answer;
+	}
 }
