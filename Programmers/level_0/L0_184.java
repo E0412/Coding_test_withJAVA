1169025d8408a23a63eee9f301c425e4f690fa59
gayoung choi <cy614@naver.com>
2026-07-24 23:20:27 +0900

    modify : PG0 - 분수의 덧셈

--- Programmers/level_0/L0_184.java
+++ Programmers/level_0/L0_184.java
@@ -14,28 +14,10 @@ public class L0_184 {
 	public int[] solution(int numer1, int denom1, int numer2, int denom2) {
 		int[] answer = new int[2];
 
-		int max = Math.max(denom1, denom2);
-		int min = Math.min(denom1, denom2);
-		int d = max / min;
-
-		int n = gcd(min, max);
-		//수정 : 최대 공약수와 최소 공배수를 이용
-		//배수 관계인 경우 : 최대공약수
-		if(max % min == 0) {
-			if(denom1 > denom2) {
-				answer[0] = numer1 + numer2 * d;
-				answer[1] = max;
-			} else {
-				answer[0] = numer1 * d + numer2;
-				answer[1] = max;
-			}
-		} 
-		//배수 관계가 아닌 경우 : 최소공배수
-		else {
-			int m = denom1 * denom2 / gcd(min, max);
-			answer[0] = numer1 * (m / denom1) + numer2 * (m / denom2);
-			answer[1] = m;
-		}
+		//수정2 : 최소공배수를 이용
+		int n = denom1 * denom2 / gcd(denom1, denom2);
+		answer[0] = numer1 * (n / denom1) + numer2 * (n / denom2);
+		answer[1] = n;
 
 		int g = gcd(answer[0], answer[1]);
 		answer[0] /= g;
