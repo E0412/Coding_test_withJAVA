77bac693f39efc64e639d16d2c2249fd8f7fcaa2
gayoung choi <cy614@naver.com>
2026-07-25 23:36:52 +0900

    feat : PG2 - 피로도 (1)

---
+++ Programmers/level_2/L2_31.java
@@ -0,0 +1,22 @@
+package level_2;
+
+//피로도
+public class L2_31 {
+
+	int dp(int n) {
+
+		return n;
+	}
+
+	public int solution(int k, int[][] dungeons) {
+		int answer = -1;
+
+		for(int i = 0; i < dungeons.length; i++) {
+			for(int j = 0; j < dungeons[i].length; j++) {
+
+			}
+		}
+
+		return answer;
+	}
+}
