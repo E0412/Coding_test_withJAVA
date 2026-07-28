da14e67534c57a616fbaad80dc3d0b3cfa119dd5
gayoung choi <cy614@naver.com>
2026-07-28 23:07:02 +0900

    feat : PG2 - 주식가격 (해결)

---
+++ Programmers/level_2/L2_32.java
@@ -0,0 +1,24 @@
+package level_2;
+
+//주식가격
+public class L2_32 {
+	public int[] solution(int[] prices) {
+		int[] answer = new int[prices.length];
+
+		for(int i = 0; i < prices.length; i++) {
+			int cnt = 0;
+
+			for(int j = i + 1; j < prices.length; j++) {
+				if(prices[i] <= prices[j]) {
+					cnt++;
+				} else {
+					cnt++;
+					break;
+				}
+			}
+			answer[i] = cnt;
+		}
+
+		return answer;
+	}
+}
