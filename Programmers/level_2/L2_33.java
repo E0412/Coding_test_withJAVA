85695d7b3c1199baa9fb7f0ecfd0ef97dbefc78d
gayoung choi <cy614@naver.com>
2026-07-29 23:10:53 +0900

    feat : PG2 - 괄호 회전하기 (시도 1)

---
+++ Programmers/level_2/L2_33.java
@@ -0,0 +1,18 @@
+package level_2;
+
+import java.util.Stack;
+
+//괄호 회전하기
+public class L2_33 {
+	public int solution(String s) {
+		int answer = -1;
+
+		Stack<String> st = new Stack<>();
+
+		for(int i = 0; i < s.length(); i++) {
+
+		}
+
+		return answer;
+	}
+}
