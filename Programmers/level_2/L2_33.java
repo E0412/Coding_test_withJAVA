35699a5fb0224a682b4026d1746000fb6b3aaad8
gayoung choi <cy614@naver.com>
2026-08-01 23:31:59 +0900

    feat : PG2 - 괄호 회전하기 (Stack)

--- Programmers/level_2/L2_33.java
+++ Programmers/level_2/L2_33.java
@@ -9,26 +9,42 @@ public class L2_33 {
 		Stack<Character> st = new Stack<>();
 
 		for(char c : s.toCharArray()) {
-			if(st.isEmpty()) {
-				return false;
+			//여는 괄호면 push
+			if(c == '(' || c == '[' || c == '{') {
+				st.push(c);
+			} 
+			//닫는 괄호인 경우
+			else {
+				//stack이 비어있는 경우 false
+				if(st.isEmpty()) {
+					return false;
+				}
+				char top = st.peek(); //현재 스택
+
+				//짝이 맞으면 내보낸다
+				if(c == ')' && top == '(' || c == ']' && top == '[' || c == '}' && top == '{') {
+					st.pop();
+				}
+				//짝이 맞지 않으면 false
+				else {
+					return false;
+				}
 			}
 		}
-
-		return false;
+		return st.isEmpty(); //괄호 처리 후 스택이 비어있어야 성공
 	}
 
 	public int solution(String s) {
 		int answer = 0;
 
-
 		for(int i = 0; i < s.length(); i++) {
 			//왼쪽으로 i칸 회전
 			String r = s.substring(i)+ s.substring(0, i);
 
-
-
+			if(chk(r)) {
+				answer++;
+			}
 		}
-
 		return answer;
 	}
 }
