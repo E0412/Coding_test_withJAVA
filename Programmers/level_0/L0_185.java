8de714e49a69871e9465a867108f5b38232b4bda
gayoung choi <cy614@naver.com>
2026-08-02 15:37:13 +0900

    feat : PG0 - 겹치는 선분의 길이 (시도 1)

---
+++ Programmers/level_0/L0_185.java
@@ -0,0 +1,16 @@
+package level_0;
+
+//겹치는 선분의 길이
+public class L0_185 {
+	public int solution(int[][] lines) {
+        int answer = 0;
+        
+        int start = 0;
+        int end = 0;
+        
+         for (int i = 0; i < lines.length; i++) {
+			
+		}
+        return answer;
+    }
+}
