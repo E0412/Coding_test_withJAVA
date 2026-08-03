b0d52055244c94ec2f07db90e7ba1c3fe6a9334f
gayoung choi <cy614@naver.com>
2026-08-03 23:56:22 +0900

    modify : PG0 - 겹치는 선분의 길이 (해결)

--- Programmers/level_0/L0_185.java
+++ Programmers/level_0/L0_185.java
@@ -3,14 +3,23 @@ package level_0;
 //겹치는 선분의 길이
 public class L0_185 {
 	public int solution(int[][] lines) {
-        int answer = 0;
-        
-        int start = 0;
-        int end = 0;
-        
-         for (int i = 0; i < lines.length; i++) {
-			
+		int answer = 0;
+		int[] arr = new int[201];
+
+		for(int i = 0; i < 3; i++) {
+			int start = lines[i][0];
+			int end = lines[i][1];
+
+			for (int j = start; j < end; j++) {
+				arr[j + 100]++; //좌표 + 100 -> 마이너스 값 방지
+			}
 		}
-        return answer;
-    }
+
+		for (int i : arr) {
+			if(i >= 2) {
+				answer++;
+			}
+		}
+		return answer;
+	}
 }
