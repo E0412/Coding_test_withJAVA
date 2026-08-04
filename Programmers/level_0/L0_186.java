e6d07a7d1d529e34221e33860eb90ed6dd285656
gayoung choi <cy614@naver.com>
2026-08-04 15:12:44 +0900

    feat : PG0 - 안전지대 (시도 1)

---
+++ Programmers/level_0/L0_186.java
@@ -0,0 +1,22 @@
+package level_0;
+
+//안전지대
+public class L0_186 {
+	//대각선 좌표
+	int[] dx = {-1, -1, -1, 0, 0, 1, 1, 1};
+	int[] dy = {-1, 0, 1, -1, 1, -1, 0, 1};
+
+	public int solution(int[][] board) {
+		int answer = 0;
+
+		for(int i = 0; i < board.length; i++) {
+			for(int j = 0; j < board[i].length; j++) {
+				if(board[i][j] == 1) {
+
+				}
+			}
+		}
+
+		return answer;
+	}
+}
