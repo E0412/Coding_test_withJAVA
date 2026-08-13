efa3aab8e8d7f15930cf706a165ea48cc7064c5c
gayoung choi <cy614@naver.com>
2026-08-13 23:56:33 +0900

    modify : PG1 - 둘만의 아웃 (해결)

--- Programmers/level_1/L1_58.java
+++ Programmers/level_1/L1_58.java
@@ -5,11 +5,25 @@ public class L1_58 {
 	public String solution(String s, String skip, int index) {
 		String answer = "";
 
-		//skip과 초과하는 경우, z이후 추가 필요
 		for(char c : s.toCharArray()) {
-			answer += (char) (c + index);
-		}
+			int cnt = 0;
+			char cur = c; //현재 문자
 
+			while(cnt < index) {
+				cur++; //문자 하나씩 이동
+
+				//z를 넘어가면 다시 a로
+				if(cur > 'z') {
+					cur = 'a';
+				}
+				//skip에 있는지 검증
+				if(skip.contains(String.valueOf(cur))) {
+					continue;
+				}
+				cnt++;
+			}
+			answer += cur;
+		}
 		return answer;
 	}
 }
