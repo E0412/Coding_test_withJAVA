19cd1145c31f889daa9c5fba092b7851ded4d245
gayoung choi <cy614@naver.com>
2026-08-18 14:56:46 +0900

    modify : PG2 - 숫자 변환하기 (BFS)

--- Programmers/level_2/L2_35.java
+++ Programmers/level_2/L2_35.java
@@ -1,32 +1,38 @@
 package level_2;
 
+import java.util.*;
+
 //숫자 변환하기
 public class L2_35 {
 	public int solution(int x, int y, int n) {
 		int answer = 0;
 
-		int m = x + n;
-		
-		//while문이 끝나지 않아서 오류 발생
-		while(x != y) {
-			if(y % m == 0) {
-				if(y == m) {
-					return 1;
+		Queue<Integer> q = new LinkedList<Integer>();
+		boolean chk[] = new boolean[y + 1]; 
+
+		q.add(x);
+		chk[x] = true;
+
+		while(!q.isEmpty()) {
+
+			int cur = q.poll(); //현재 저장된 수
+
+			if(cur == y) { //값이 같으면 반환
+				return answer;
+			}
+
+			int[] add = {x + n, x * 2, x * 3}; //연산 경우의 수
+
+			for(int m : add) { //경우의 수 탐색
+				//y보다 작은경우 추가
+				if(m <= y && !chk[m]) {
+					chk[m] = true; //중복 방지
+					q.add(m);
 				}
 			}
-			else if(y % x == 0) {
-				if(y % (x * 2) == 0) {
-					x *= 2;
-					answer++;
-				} else if(y % (x * 3) == 0) {
-					x *= 3;
-					answer++;
-				}
-			}
-			else {
-				return -1;
-			}
+			answer++;
 		}
-		return answer;
+
+		return -1; 
 	}
 }
