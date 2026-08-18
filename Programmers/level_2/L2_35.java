26692ce5f92e32114ce142c23c10602f56ca2901
gayoung choi <cy614@naver.com>
2026-08-18 15:08:00 +0900

    modify : PG2 - 숫자 변환하기 (BFS), 오류 수정

--- Programmers/level_2/L2_35.java
+++ Programmers/level_2/L2_35.java
@@ -14,25 +14,28 @@ public class L2_35 {
 		chk[x] = true;
 
 		while(!q.isEmpty()) {
+			int size = q.size();
 
-			int cur = q.poll(); //현재 저장된 수
+			for (int i = 0; i < size; i++) {
+				int cur = q.poll(); //현재 저장된 수
 
-			if(cur == y) { //값이 같으면 반환
-				return answer;
-			}
+				if(cur == y) { //값이 같으면 반환
+					return answer;
+				}
 
-			int[] add = {x + n, x * 2, x * 3}; //연산 경우의 수
+				//연산 경우의 수 -> 현재 값으로 갱신
+				int[] add = {cur + n, cur * 2, cur * 3}; 
 
-			for(int m : add) { //경우의 수 탐색
-				//y보다 작은경우 추가
-				if(m <= y && !chk[m]) {
-					chk[m] = true; //중복 방지
-					q.add(m);
+				for(int m : add) { //경우의 수 탐색
+					//y보다 작은경우 추가
+					if(m <= y && !chk[m]) {
+						chk[m] = true; //중복 방지
+						q.add(m);
+					}
 				}
 			}
 			answer++;
 		}
-
 		return -1; 
 	}
 }
