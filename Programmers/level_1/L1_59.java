be56e0ae97b0868c6a234bd01c6257113e066a3b
gayoung choi <cy614@naver.com>
2026-08-20 23:58:19 +0900

    feat : PG1 - 실패율 (시도 1)

---
+++ Programmers/level_1/L1_59.java
@@ -0,0 +1,16 @@
+package level_1;
+
+//실패율
+public class L1_59 {
+	public int[] solution(int N, int[] stages) {
+		int[] answer = new int[N + 1];
+
+		//도달했으나 클리어하지 못한 플레이어 수 / 스테이지에 도달한 플레이어 수
+		int fail = 0;
+
+		
+		
+		
+		return answer;
+	}
+}
