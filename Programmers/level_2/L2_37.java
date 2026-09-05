76c2f0392227267ea381dbb3ce9cbc1bf17bcf25
gayoung choi <cy614@naver.com>
2026-09-05 23:59:15 +0900

    modify : PG2 - 더 맵게 (해결)

--- Programmers/level_2/L2_37.java
+++ Programmers/level_2/L2_37.java
@@ -7,23 +7,28 @@ public class L2_37 {
 	public int solution(int[] scoville, int K) {
 		int answer = 0;
 
-		List<Integer> list = new LinkedList<Integer>();
-		for (int i = 0; i < scoville.length; i++) {
-			list.add(scoville[i]);
-		}
-		Collections.sort(list);
+		PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
 
-		//수정 필요, long 자료형 고려하기 모든 음식의 스코빌 지수를 K 이상으로 만들 수 없는 경우에는 -1 로직 추가하기
-		for (int i = 0; i < list.size(); i++) {
-			if(list.get(0) < K) {
-				list.remove(0);
-				list.remove(1);
-				list.add(list.get(0) + list.get(1) * list.get(1)); //새로운 음식
-				answer++;
-				Collections.sort(list);
-			} else {
-				break;
+		for(int n : scoville) {
+			pq.add(n);
+		}
+
+		//가장 작은 음식이 K 이상이면 종료 
+		while(pq.peek() < K) {
+			//음식이 2개 미만인 경우 -1 반환
+			if(pq.size() < 2) {
+				return -1;
 			}
+			//가장 작은 음식
+			int first = pq.poll();
+
+			//두번째로 작은 음식
+			int second = pq.poll();
+
+			int food = first + (second * 2);
+			pq.add(food);
+
+			answer++; //횟수 증가 
 		}
 		return answer;
 	}
