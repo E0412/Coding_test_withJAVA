30119cd19ba499e7ad3f7bf87f6055d899c7f559
gayoung choi <cy614@naver.com>
2026-09-15 17:09:15 +0900

    modify : PG1 - [PCCE 기출문제] 9번 / 이웃한 칸 (해결)

--- Programmers/level_1/L1_63.java
+++ Programmers/level_1/L1_63.java
@@ -4,8 +4,9 @@ package level_1;
 public class L1_63 {
 	public int solution(String[][] board, int h, int w) {
 		int answer = 0; //같은 색으로 색칠된 칸의 개수 
-		board = new String[h+1][w+1];
-		int n = board.length;
+		//초기화 배열 삭제, 입력된 배열 이용
+		int n = board.length; //행 비교
+		int m = board[0].length; //열 비교
 
 		int[] dh = {0, 1, -1, 0};
 		int[] dw = {1, 0, 0, -1};
@@ -14,8 +15,10 @@ public class L1_63 {
 			int h_check = h + dh[i];
 			int w_check = w + dw[i];
 
-			if(h_check >= 0 && h_check < n && w_check >= 0 && w_check < n) {
-				if(board[h][w] == board[h_check][w_check]) {
+			//수정 : w_check는 board[0].length와 비교
+			if(h_check >= 0 && h_check < n && w_check >= 0 && w_check < m) {
+				//수정 : equals로 비교
+				if(board[h][w].equals(board[h_check][w_check])) {
 					answer++;
 				}
 			}
