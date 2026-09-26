b99ebce554c086286e808b23b234d91d9f6ffeef
gayoung choi <cy614@naver.com>
2026-09-26 23:44:36 +0900

    modify : PG1 - 체육복 (해결)

--- Programmers/level_1/L1_65.java
+++ Programmers/level_1/L1_65.java
@@ -32,13 +32,23 @@ public class L1_65 {
 
 		//체육복 분배
 		for(int i = 0; i < n; i++) {
-
-
+			//왼쪽에서 빌리는 케이스
+			if(i > 0 && arr[i - 1] == 2) {
+				if(arr[i] == 0) {
+					arr[i] = 1;
+					arr[i - 1] = 1;
+				}
+			}
+			//오른쪽
+			else if(i < n - 1 && arr[i + 1] == 2) {
+				if(arr[i] == 0) {
+					arr[i] = 1;
+					arr[i + 1] = 1;
+				}
+			}
 		}
-
-
 		for(int i : arr) {
-			if(i == 1) answer++;
+			if(i >= 1) answer++;
 		}
 
 		return answer;
